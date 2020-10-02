2faaa8238f08f90c75c593f48b2898b53faeb61d
Jorge Izquierdo <jorge@aragon.one>
2020-10-02 05:51:45 +0200

    NewFactory event

--- contracts/EagletRegistry.sol
+++ contracts/EagletRegistry.sol
@@ -6,13 +6,19 @@ pragma solidity 0.6.8;
 
 import "erc3k/contracts/ERC3000Executor.sol";
 
-contract EagletRegistry {
+import "./lib/MiniACL.sol";
+
+contract EagletRegistry is MiniACL {
     mapping (string => bool) public nameUsed;
 
+    event NewFactory(address indexed factory);
     event Registered(ERC3000Executor indexed dao, address indexed registrant, string name);
     event SetMetadata(ERC3000Executor indexed dao, bytes metadata);
 
-    function register(ERC3000Executor _dao, string calldata _name, bytes calldata _initialMetadata) external {
+    function register(ERC3000Executor _dao, string calldata _name, bytes calldata _initialMetadata)
+        auth(this.register.selector)
+        external
+    {
         require(!nameUsed[_name], "registry: name used");
         
         // all will revert if `_dao` is not interface compliant in _setMetadata
