a7016fea2c35a27a4d4328ef805d897e0e810005
Enrique Ortiz <hi@enriqueortiz.dev>
2020-10-01 20:32:05 -0400

    Add ABI syncing script

---
+++ scripts/sync-abis.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+
+const KNOWN_ABIS = [
+  'Eaglet.json',
+  'EagletFactory.json',
+  'OptimisticQueue.json',
+  'OptimisticQueueFactory.json',
+]
+
+async function main() {
+  KNOWN_ABIS.map((abiName) => {
+    console.log(`Syncing ${abiName}`)
+    console.log(fs.existsSync(`artifacts/${abiName}`), `found ${abiName}`)
+    const rawAbiFile = fs.readFileSync(`artifacts/${abiName}`)
+    const parsedAbiFile = JSON.parse(rawAbiFile)
+    const abiContent = parsedAbiFile.abi
+
+    const stringifiedAbi = JSON.stringify(abiContent)
+    fs.writeFileSync(`subgraph/abis/${abiName}`, stringifiedAbi)
+  })
+
+  console.log('All ABIs synced!')
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
