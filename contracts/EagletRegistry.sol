719eaadad9403fc5cdb4a54cae7323591a3dfbdc
Jorge Izquierdo <jorge@aragon.one>
2020-10-02 05:16:57 +0200

    Eaglet Registry (#17)
    * ERC-3000 configure
    
    * Collateral management in queue
    
    * Remove unusued import
    
    * clean
    
    * setState
    
    * clean
    
    * Arbitrate
    
    * Move data structure hashing to ERC3000Data
    
    * DepositLib
    
    * Fixes
    
    * Factory scaffold
    
    * MiniACL.bulk
    
    * Factory: dummy eaglet
    
    * Failing test: code too large
    
    * Split OptimisticQueueFactory
    
    * Run all tests
    
    * Gas
    
    * Optimize and test gas
    
    * Fix covarage
    
    * Eaglet Registry
    
    * Merge

---
+++ contracts/EagletRegistry.sol
@@ -0,0 +1,34 @@
+/*
+ * SPDX-License-Identifier:    GPL-3.0
+ */
+
+pragma solidity 0.6.8;
+
+import "erc3k/contracts/ERC3000Executor.sol";
+
+contract EagletRegistry {
+    mapping (string => bool) public nameUsed;
+
+    event Registered(ERC3000Executor indexed dao, address indexed registrant, string name);
+    event SetMetadata(ERC3000Executor indexed dao, bytes metadata);
+
+    function register(ERC3000Executor _dao, string calldata _name, bytes calldata _initialMetadata) external {
+        require(!nameUsed[_name], "registry: name used");
+        
+        // all will revert if `_dao` is not interface compliant in _setMetadata
+        nameUsed[_name] = true;
+
+        emit Registered(_dao, msg.sender, _name);
+        _setMetadata(_dao, _initialMetadata);
+    }
+
+    function setMetadata(bytes memory _metadata) public {
+        _setMetadata(ERC3000Executor(msg.sender), _metadata);
+    }
+
+    function _setMetadata(ERC3000Executor _dao, bytes memory _metadata) internal {
+        require(_dao.supportsInterface(_dao.exec.selector), "registry: bad interface");
+
+        emit SetMetadata(_dao, _metadata);
+    }
+}
--- test/eaglet-factory.js
+++ test/eaglet-factory.js
@@ -1,11 +1,13 @@
 const { expect } = require("chai")
 
 const EVENTS = {
-  NEW: 'NewEaglet'
+  NEW: 'NewEaglet',
+  REGISTERED: 'Registered',
+  SET_METADATA: 'SetMetadata'
 }
 
 describe('EagletFactory', function () {
-  let signers, owner, eagletFactory
+  let signers, owner, eagletFactory, eagletRegistry
 
   before(async () => {
     signers = await ethers.getSigners()
@@ -14,21 +16,27 @@ describe('EagletFactory', function () {
 
   beforeEach(async () => {
     const OptimisticQueueFactory = await ethers.getContractFactory('OptimisticQueueFactory')
+    const EagletRegistry = await ethers.getContractFactory('EagletRegistry')
     const EagletFactory = await ethers.getContractFactory('EagletFactory')
 
     queueFactory = await OptimisticQueueFactory.deploy()
-    eagletFactory = await EagletFactory.deploy(queueFactory.address)
+    eagletRegistry = await EagletRegistry.deploy()
+    eagletFactory = await EagletFactory.deploy(queueFactory.address, eagletRegistry.address)
   })
 
   const GAS_TARGET = !process.env.SOLIDITY_COVERAGE ? 4e6 : 20e6
   it(`deploys DAO under ${GAS_TARGET} gas`, async () => {
-    const tx = eagletFactory.newDummyEaglet()
-    await expect(tx)
-      .to.emit(eagletFactory, EVENTS.NEW)
+    const tx = eagletFactory.newDummyEaglet('eagle')
+
+    await expect(tx).to.emit(eagletFactory, EVENTS.NEW)
+    await expect(tx).to.emit(eagletRegistry, EVENTS.REGISTERED)
+    await expect(tx).to.emit(eagletRegistry, EVENTS.SET_METADATA)
 
     const { hash } = await tx
     const { gasUsed } = await waffle.provider.getTransactionReceipt(hash)
 
     expect(gasUsed).to.be.lte(GAS_TARGET)
+
+    console.log('gas used:', gasUsed.toNumber())
   })
 })
