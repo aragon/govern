ab2c0f3abf722bdca0dfd0ab18e9cbf6e1718eae
Jorge Izquierdo <jorge@aragon.one>
2020-10-02 08:13:15 +0200

    Deploy eaglet on CD

--- buidler.config.js
+++ buidler.config.js
@@ -4,6 +4,15 @@ usePlugin("@nomiclabs/buidler-etherscan")
 usePlugin("@nomiclabs/buidler-waffle")
 
 require('dotenv').config()
+const { readFileSync, writeFileSync } = require('fs')
+const { uniqueNamesGenerator, adjectives, colors, animals } = require('unique-names-generator')
+
+const FACTORY_CACHE_NAME = 'eaglet-factory-rinkeby'
+const REGISTER_EVENT_NAME = 'Registered'
+const REGISTRY_EVENTS_ABI = [
+  'event Registered(address indexed dao, address queue, address indexed registrant, string name)',
+  'event SetMetadata(address indexed dao, bytes metadata)'
+]
 
 task("accounts", "Prints the list of accounts", async () => {
   const accounts = await ethers.getSigners();
@@ -11,33 +20,69 @@ task("accounts", "Prints the list of accounts", async () => {
   for (const account of accounts) {
     console.log(await account.getAddress());
   }
-});
+})
 
-task("deploy", "Deploys an eaglet instance").setAction(
+const print = ({ address }, name) =>
+  console.log(`${name}: ${address} (https://rinkeby.etherscan.io/address/${address})`)
+
+task("deploy-registry", "Deploys an ERC3000Registry instance").setAction(
   async (_, { ethers }) => {
-    const optimisticQueueFactory = await ethers.getContractFactory(
-      "OptimisticQueueFactory"
-    );
-    const eagletFactory = await ethers.getContractFactory("EagletFactory");
-
-    const queueFactoryTx = await optimisticQueueFactory.deploy();
-    console.log("queueFactory deployed: ", queueFactoryTx.address);
-
-    const eagletFactoryTx = await eagletFactory.deploy(queueFactoryTx.address);
-    console.log("eagletFactory deployed: ", eagletFactoryTx.address);
-
-    console.log("Done!");
+    const ERC3000Registry = await ethers.getContractFactory("ERC3000Registry")
+    print(await ERC3000Registry.deploy(), 'ERC3000Registry')
   }
-);
+)
 
-task("deploy-registry", "Deploys ERC3000Registry").setAction(
+task("deploy-factory", "Deploys an EagletFactory instance").setAction(
   async (_, { ethers }) => {
-    const ERC3000Registry = await ethers.getContractFactory("ERC3000Registry");
-    const registry = await ERC3000Registry.deploy()
+    const OptimisticQueueFactory = await ethers.getContractFactory("OptimisticQueueFactory")
+    const EagletFactory = await ethers.getContractFactory("EagletFactory")
 
-    console.log(`ERC3000Registry: ${registry.address} (https://rinkeby.etherscan.io/address/${registry.address})`);
+    const queueFactory = await OptimisticQueueFactory.deploy()
+    print(queueFactory, 'OptimisticQueueFactory')
+
+    const eagletFactory = await EagletFactory.deploy(process.env.REGISTRY_RINKEBY, queueFactory.address)
+    print(eagletFactory, 'EagletFactory')
+
+    if (process.env.CD) {
+      writeFileSync(FACTORY_CACHE_NAME, eagletFactory.address)
+    }
   }
-);
+)
+
+task("deploy-eaglet", "Deploys an Eaglet from provided factory")
+  .addOptionalParam('factory', 'Factory address')
+  .addOptionalParam('useProxies', 'Whether to deploy eaglet with proxies')
+  .addOptionalParam('name', 'DAO name (must be unique at Registry level)')
+  .setAction(async ({ factory: factoryAddr, useProxies, name }, { ethers }) => {
+    factoryAddr = factoryAddr || process.env.FACTORY_RINKEBY || readFileSync(FACTORY_CACHE_NAME).toString()
+    name = name
+      || uniqueNamesGenerator({
+        dictionaries: [adjectives, colors, animals],
+        length: 2,
+        separator: '-'
+      })
+
+    if (!factoryAddr) {
+      return console.error('Please provide factory address as --factory [addr] or add as FACTORY_[NETWORK] to your environment')
+    }
+
+    let registryInterface = new ethers.utils.Interface(REGISTRY_EVENTS_ABI)
+
+    const eagletFactory = await ethers.getContractAt('EagletFactory', factoryAddr)
+    const tx = await eagletFactory.newDummyEaglet(name)
+
+    const { events } = await tx.wait()
+
+    const { args: { dao, queue }} = events
+      .filter(({ address }) => address === process.env.REGISTRY_RINKEBY)
+      .map(log => registryInterface.parseLog(log))
+      .find(({ name }) => name === REGISTER_EVENT_NAME)
+
+    console.log(`A wild new Eaglet named '${name}' appeared 🐥`)
+    print({ address: dao }, 'Eaglet')
+    print({ address: queue }, 'OptimisticQueue')
+  }
+)
 
 const ETH_KEY = process.env.ETH_KEY
 const accounts = ETH_KEY ? ETH_KEY.split(",") : [""]
--- test/erc3000-registry.js
+++ test/erc3000-registry.js
@@ -30,9 +30,7 @@ describe('ERC3000 Registry', function () {
     await expect(tx).to.emit(registry, EVENTS.REGISTERED)
     await expect(tx).to.emit(registry, EVENTS.SET_METADATA)
 
-    const { hash } = await tx
-    const { gasUsed } = await waffle.provider.getTransactionReceipt(hash)
-
+    const { gasUsed } = await tx.wait()
     expect(gasUsed).to.be.lte(GAS_TARGET)
 
     console.log('gas used:', gasUsed.toNumber())
