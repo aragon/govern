7fde1844039d5ef7a02add085b8f489ccb5d8952
Jorge Izquierdo <jorge@aragon.one>
2020-10-08 06:32:26 +0200

    Add erc3k to monorepo (#27)

---
+++ packages/erc3k/contracts/ERC3000.sol
@@ -0,0 +1,25 @@
+/*
+ * SPDX-License-Identifier:    MIT
+ */
+
+pragma solidity ^0.6.8;
+pragma experimental ABIEncoderV2;
+
+import "./lib/erc165/ERC165.sol";
+
+import "./IERC3000.sol";
+
+abstract contract ERC3000 is IERC3000, ERC165 {
+    bytes4 internal constant ERC3000_INTERFACE_ID =
+        this.schedule.selector
+        ^ this.execute.selector
+        ^ this.challenge.selector
+        ^ this.resolve.selector
+        ^ this.veto.selector
+        ^ this.configure.selector
+    ;
+
+    function supportsInterface(bytes4 _interfaceId) virtual override public pure returns (bool) {
+        return _interfaceId == ERC3000_INTERFACE_ID || super.supportsInterface(_interfaceId);
+    }
+}
