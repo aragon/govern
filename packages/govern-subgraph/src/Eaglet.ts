dda81fceec2d347f3a253893c79c0afd9be6dcb6
0xGabi <gabrielpk.18@gmail.com>
2020-10-08 06:21:28 -0300

    Eaglet: add new eagley mapping

--- packages/govern-subgraph/src/Eaglet.ts
+++ packages/govern-subgraph/src/Eaglet.ts
@@ -1,97 +1,112 @@
-import { Address } from '@graphprotocol/graph-ts'
+import { Address, ethereum } from "@graphprotocol/graph-ts";
 import {
+  Executed as ExecutedEvent,
   Frozen as FrozenEvent,
   Granted as GrantedEvent,
   Revoked as RevokedEvent,
-} from '../generated/templates/Govern/Govern'
+} from "../generated/templates/Eaglet/Eaglet";
 import {
-  Govern,
-  Role
-} from '../generated/schema'
+  Action as ActionEntity,
+  Eaglet as EagletEntity,
+  Execution as ExecutionEntity,
+} from "../generated/schema";
+import { frozenRoles, roleGranted, roleRevoked } from "./lib/MiniACL";
 
-const FREEZE_ADDR = '0x0000000000000000000000000000000000000001'
+export function handleExecuted(event: ExecutedEvent): void {
+  const eaglet = loadOrCreateEaglet(event.address);
 
-export function handleFrozen(event: FrozenEvent): void {
-  const govern = Govern.load(event.address.toHexString())
-  let id = 0
-  const roles = govern.roles
-  for (id = 0; id < roles.length; id++) {
-    const currentRole = roles[id]
-    const funcSelector = currentRole.split('-')[1]
-    if (funcSelector === event.params.role.toHexString()) {
-      const role = Role.load(currentRole)
-      const freezeBytes = Address.fromString(FREEZE_ADDR)
-      role.who = freezeBytes
-      role.save()
-      break
-    }
-  }
-  govern.save()
+  const execution = loadOrCreateExecution(event);
+
+  createActions(event);
+
+  // add the execution
+  const currentExecutions = eaglet.executions;
+  currentExecutions.push(execution.id);
+  eaglet.executions = currentExecutions;
+
+  execution.save();
+  eaglet.save();
 }
 
-export function handleRevoked(event: RevokedEvent): void {
-  let govern = Govern.load(event.address.toHexString())
-  const roleId =
-    event.address.toHexString() +
-    '-' +
-    event.params.role.toHexString() +
-    '-' +
-    event.params.who.toHexString()
-  let role = Role.load(roleId)
-  if (!role) {
-    role = new Role(roleId)
-    role.role = event.params.role
-    role.who = event.params.who
-  }
-  role.revoked = true
-  // Check if role exists in roles array and if not,
-  // push it
-  const roles = govern.roles
-  let id = 0
-  let exists = false
-  for(id = 0; id < roles.length; id++) {
-    const currentRole = roles[id]
-    if (currentRole === roleId) {
-      exists = true
-    }
-  }
+// MiniACL Events
 
-  if (!exists) {
-    roles.push(roleId)
-    govern.roles = roles
-  }
-  role.save()
-  govern.save()
+export function handleFrozen(event: FrozenEvent): void {
+  const eaglet = loadOrCreateEaglet(event.address);
+
+  const roles = eaglet.roles!
+
+  frozenRoles(roles, event.params.role);
 }
 
 export function handleGranted(event: GrantedEvent): void {
-  const govern = Govern.load(event.address.toHexString())
-  // roleID = contract address + role itself,
-  // which will be the function selector + who
-  // This is equivalent to storing all roles in the contract, and looking up the corresponding
-  // entry by mapping role => who
-  const roleId =
-    event.address.toHexString() +
-    '-' +
-    event.params.role.toHexString() +
-    '-' +
-    event.params.who.toHexString()
-  // We MUST first try to load this event because you can "grant" the role
-  // to the same addr many times, even if it has no effect.
-  let role = Role.load(roleId)
-  let exists = true
-  if (!role) {
-    exists = false
-    role = new Role(roleId)
+  const eaglet = loadOrCreateEaglet(event.address);
+
+  // contemplar el caso en que se crea un nueva cola
+
+  const role = roleGranted(event.address, event.params.role, event.params.who);
+
+  // add the role
+  const currentRoles = eaglet.roles;
+  currentRoles.push(role.id);
+  eaglet.roles = currentRoles;
+
+  eaglet.save();
+}
+
+export function handleRevoked(event: RevokedEvent): void {
+  const eaglet = loadOrCreateEaglet(event.address);
+
+  const role = roleRevoked(event.address, event.params.role, event.params.who);
+
+  // add the role
+  const currentRoles = eaglet.roles;
+  currentRoles.push(role.id);
+  eaglet.roles = currentRoles;
+
+  eaglet.save();
+}
+
+// Helpers
+
+export function loadOrCreateEaglet(entity: Address): EagletEntity {
+  const eagletId = entity.toHexString();
+  // Create eaglet
+  let eaglet = EagletEntity.load(eagletId);
+  if (eaglet === null) {
+    eaglet = new EagletEntity(eagletId);
+    eaglet.address = entity;
+    eaglet.executions = [];
+    eaglet.roles = [];
   }
-  role.role = event.params.role
-  role.who = event.params.who
-  role.revoked = false
-  const roles = govern.roles
-  if (!exists) {
-    roles.push(roleId)
+  return eaglet!;
+}
+
+function buildId(event: ethereum.Event): string {
+  return event.transaction.hash.toHexString() + event.logIndex.toString();
+}
+
+function loadOrCreateExecution(event: ExecutedEvent): ExecutionEntity {
+  const executionId = buildId(event);
+  // Create execution
+  let execution = ExecutionEntity.load(executionId);
+  if (execution === null) {
+    execution = new ExecutionEntity(executionId);
+    execution.sender = event.params.actor;
+    execution.results = event.params.execResults;
   }
-  role.save()
-  govern.roles = roles
-  govern.save()
+  return execution!;
+}
+
+function createActions(event: ExecutedEvent): void {
+  event.params.actions.forEach((actionData, index) => {
+    const actionId = buildId(event) + "-action-" + index.toString();
+    const action = new ActionEntity(actionId);
+
+    action.execution =  buildId(event);
+    action.to = actionData.to;
+    action.value = actionData.value;
+    action.data = actionData.data;
+
+    action.save();
+  });
 }
