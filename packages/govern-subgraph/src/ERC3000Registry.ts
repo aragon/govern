dcb417cc0007617dbafb4f7acc27dc2c2372155b
0xGabi <gabrielpk.18@gmail.com>
2020-10-08 06:22:08 -0300

    Registry: update ERC300Registry mapping

---
+++ packages/govern-subgraph/src/ERC3000Registry.ts
@@ -0,0 +1,54 @@
+import { Address } from "@graphprotocol/graph-ts";
+import {
+  Registered as RegisteredEvent,
+  SetMetadata as SetMetadataEvent,
+} from "../generated/ERC3000Registry/ERC3000Registry";
+import {
+  Eaglet as EagletTemplate,
+  OptimisticQueue as QueueTemplate,
+} from "../generated/templates";
+import {
+  ERC3000Registry as ERC3000RegistryEntity,
+  Entry as EntryEntity
+} from "../generated/schema";
+
+export function handleRegistered(event: RegisteredEvent): void {
+  const registry = loadOrCreateRegistry(event.address)
+
+  const entry = new EntryEntity(event.params.name)
+
+  entry.name = event.params.name
+  entry.dao = event.params.dao.toHexString()
+  entry.queue = event.params.queue.toHexString()
+
+  // add entry to the registry
+  const currentEntries = registry.entries
+  currentEntries.push(entry.id)
+  registry.entries = currentEntries
+
+  entry.save()
+  registry.save()
+
+  // Create datasource templates
+  EagletTemplate.create(event.params.dao);
+  QueueTemplate.create(event.params.queue);
+}
+
+export function handleSetMetadata(event: SetMetadataEvent): void {
+  // TODO:
+}
+
+
+export function loadOrCreateRegistry(
+  registryAddress: Address
+): ERC3000RegistryEntity {
+  const registryId = registryAddress.toHexString()
+  let registry = ERC3000RegistryEntity.load(registryId)
+  if (registry === null) {
+    registry = new ERC3000RegistryEntity(registryId)
+    registry.address = registryAddress
+    registry.count = 0
+    registry.entries = []
+  }
+  return registry!
+}
