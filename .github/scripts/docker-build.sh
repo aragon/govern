dcb4926d587dcbff93ede551ad33a4134e908fef
Martynas Prokopas <promaty@gmail.com>
2020-10-15 17:23:38 +0200

    added path argument to docker build

--- .github/scripts/docker-build.sh
+++ .github/scripts/docker-build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # use previously built image for cache if possible
-REPO="$1"
-GITHUB_SHA="$2"
+PATH="$1"
+REPO="$2"
+GITHUB_SHA="$3"
 docker pull $REPO:latest || true
 docker pull $REPO:rolling || true
-docker build . -t $REPO:${GITHUB_SHA} -t $REPO:rolling --cache-from $REPO:rolling --cache-from $REPO:latest
+docker build $PATH -t $REPO:${GITHUB_SHA} -t $REPO:rolling --cache-from $REPO:rolling --cache-from $REPO:latest
 docker push $REPO:${GITHUB_SHA}
 docker push $REPO:rolling
