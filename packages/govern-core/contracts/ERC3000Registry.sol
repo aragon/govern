bd149c121e009cff360bea9544ac96ae3144f761
Gabriel Garcia <gabrielpk.18@gmail.com>
2020-10-15 10:21:50 -0300

    📦 NEW: generate abi files using buidler-extract (#52)

--- packages/govern-core/contracts/ERC3000Registry.sol
+++ packages/govern-core/contracts/ERC3000Registry.sol
@@ -12,28 +12,28 @@ contract ERC3000Registry {
 
     mapping (string => bool) public nameUsed;
 
-    event Registered(ERC3000Executor indexed dao, ERC3000 queue, address indexed registrant, string name);
-    event SetMetadata(ERC3000Executor indexed dao, bytes metadata);
+    event Registered(ERC3000Executor indexed executor, ERC3000 queue, address indexed registrant, string name);
+    event SetMetadata(ERC3000Executor indexed executor, bytes metadata);
 
-    function register(ERC3000Executor _dao, ERC3000 _queue, string calldata _name, bytes calldata _initialMetadata) external
+    function register(ERC3000Executor _executor, ERC3000 _queue, string calldata _name, bytes calldata _initialMetadata) external
     {
         require(!nameUsed[_name], "registry: name used");
         require(_queue.supportsInterface(ERC3000_INTERFACE_ID), "registry: bad interface queue");
 
-        // all will revert if `_dao` is not interface compliant in _setMetadata
+        // all will revert if `_executor` is not interface compliant in _setMetadata
         nameUsed[_name] = true;
 
-        emit Registered(_dao, _queue, msg.sender, _name);
-        _setMetadata(_dao, _initialMetadata);
+        emit Registered(_executor, _queue, msg.sender, _name);
+        _setMetadata(_executor, _initialMetadata);
     }
 
     function setMetadata(bytes memory _metadata) public {
         _setMetadata(ERC3000Executor(msg.sender), _metadata);
     }
 
-    function _setMetadata(ERC3000Executor _dao, bytes memory _metadata) internal {
-        require(_dao.supportsInterface(_dao.exec.selector), "registry: bad interface dao");
+    function _setMetadata(ERC3000Executor _executor, bytes memory _metadata) internal {
+        require(_executor.supportsInterface(_executor.exec.selector), "registry: bad interface executor");
 
-        emit SetMetadata(_dao, _metadata);
+        emit SetMetadata(_executor, _metadata);
     }
 }
