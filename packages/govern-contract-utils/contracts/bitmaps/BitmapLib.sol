000110464e3978c2304fdc360326c395ca99410d
Jorge Izquierdo <jorge@aragon.one>
2020-10-19 08:44:27 +0200

    Add tests

--- packages/govern-contract-utils/contracts/bitmaps/BitmapLib.sol
+++ packages/govern-contract-utils/contracts/bitmaps/BitmapLib.sol
@@ -9,11 +9,11 @@ library BitmapLib {
     bytes32 constant internal allowAll = empty;
     bytes32 constant internal denyAll = bytes32(uint256(-1));
 
-    function get(bytes32 map, uint8 index) public pure returns (bool) {
-        return bool(uint256(map) >> index & 1 == 1);
+    function flip(bytes32 map, uint8 index) internal pure returns (bytes32) {
+        return bytes32(uint256(map) ^ uint256(1) << index);
     }
 
-    function flip(bytes32 map, uint8 index) public pure returns (bytes32) {
-        return bytes32(uint256(map) ^ uint256(1) << index);
+    function get(bytes32 map, uint8 index) internal pure returns (bool) {
+        return bool(uint256(map) >> index & 1 == 1);
     }
 }
---
+++ packages/govern-contract-utils/test/bitmaplib.js
@@ -0,0 +1,30 @@
+const { expect } = require('chai')
+
+describe.only('Bitmap', function () {
+  let lib
+
+  beforeEach(async () => {
+    const BitmapLib = await ethers.getContractFactory('BitmapLibMock')
+    lib = await BitmapLib.deploy()
+  })
+
+  it('flips', async () => {
+    expect(await lib.flip(await lib.empty(), 1)).to.equal(`0x${'00'.repeat(31)}02`)
+  })
+
+  it('randomly flip and check', async () => {
+    let map = []
+    let bitmap = `0x${'00'.repeat(32)}`
+
+    for (let i = 0; i < 256; i++) {
+      const bit = Math.random() >= 0.5
+      map.push(bit)
+      
+      bitmap = bit ? await lib.flip(bitmap, i) : bitmap
+    }
+
+    for (let i = 0; i < 256; i++) {
+      expect(await lib.get(bitmap, i)).to.equal(map[i])
+    }
+  })
+})
