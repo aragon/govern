cbd7bdf59757edc5147d408a2f387c69cfe4a114
Martynas Prokopas <promaty@gmail.com>
2020-10-20 17:50:16 +0200

    fixed docker build script

--- .github/scripts/docker-build.sh
+++ .github/scripts/docker-build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # use previously built image for cache if possible
-PATH="$1"
+DOCKERFILE_PATH="$1"
 REPO="$2"
 GITHUB_SHA="$3"
 docker pull $REPO:latest || true
 docker pull $REPO:rolling || true
-docker build $PATH -t $REPO:${GITHUB_SHA} -t $REPO:rolling --cache-from $REPO:rolling --cache-from $REPO:latest
+docker build $DOCKERFILE_PATH -t $REPO:${GITHUB_SHA} -t $REPO:rolling --cache-from $REPO:rolling --cache-from $REPO:latest
 docker push $REPO:${GITHUB_SHA}
 docker push $REPO:rolling
