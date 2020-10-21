aa5da092907161c89ceda08b636c32222b3813c5
nivida <samuel@ethereum.org>
2020-10-21 12:24:02 +0200

    basic Configuration object created

--- packages/govern/internal/configuration/Configuration.ts
+++ packages/govern/internal/configuration/Configuration.ts
@@ -1,4 +1,41 @@
-
+/**
+ * @class Configuration
+ */
 export default class Configuration {
+  /**
+   * Holds the validated configuration object
+   *
+   * @var config
+   *
+   * @private
+   */
+  private config: any
 
+  /**
+   * @param {Object} config
+   *
+   * @constructor
+   */
+  constructor(config: any) {
+    this.setConfig(config)
+  }
+
+  /**
+   * TODO: Use a ConfigurationError object
+   *
+   * Does set and parse the given configuration object
+   *
+   * @method setConfig
+   *
+   * @returns {void}
+   *
+   * @private
+   */
+  private setConfig(config: any): void {
+    if (!config.governURL) {
+      throw new Error('Missing Govern server URL!')
+    }
+
+    this.config = config
+  }
 }
