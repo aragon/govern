b701d3188800cc0ae1047bac8359f9bae5cfa481
nivida <samuel@ethereum.org>
2020-10-21 10:49:52 +0200

    initial package structure for govern.js

---
+++ packages/govern/jest.config.js
@@ -0,0 +1,43 @@
+// TODO: Improve tests and add the branch property to the coverageThreshold config.
+const config = {
+  // notifyMode: 'success-change',
+  collectCoverage: true,
+  coverageDirectory: './coverage/',
+  coverageThreshold: {
+    global: {
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  // notify: true,
+  clearMocks: true,
+  resetMocks: true,
+  resetModules: true,
+  testMatch: ['/**/**Test.js'],
+  transform: {
+    '^.+\\.js$': '<rootDir>/../../jest.preprocessor.js'
+  },
+  bail: true,
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'dist',
+  ]
+};
+
+/**
+ * Returns the jest configuration with the moduleNameMapper object set.
+ *
+ * @param {Object} moduleNameMapper
+ *
+ * @returns {Object}
+ */
+module.exports = (moduleNameMapper) => {
+  if (moduleNameMapper) {
+    config['moduleNameMapper'] = moduleNameMapper;
+
+    return config;
+  }
+
+  return config;
+};
---
+++ packages/govern/scripts/dev/prepare.package.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# --------------------------------------------------------------------
+# Prepares package folder
+# --------------------------------------------------------------------
+
+# Creates temporary package folder
+rm -rf ./package
+mkdir package
+
+# Copy internal files to package
+cp -r ./internal ./package/
+
+# Copy public files to package
+cp -r ./public/* ./package/
+
+# Copy `package.json` to package
+cp ./package.json ./package/package.json
+
+# Copy `README.md` to package
+cp ./README.md ./package/README.md
+
+# Copy `tsconfig.prod.json`
+cp ./tsconfig.prod.json ./package/
+
+# Go to package directory
+cd ./package
+
+# Build package
+yarn build:prod
+
+# Remove not required files
+rm -rf ./index.js
+rm -rf ./tsconfig.prod.json
+
+# Move `dist` files to root folder and delete the `dist` folder
+mv ./dist/package/* ./
+
+rm -rf ./dist
+
+
+echo -e "\033[0;32mPackage successfully prepared!\033[0m"
