2e1261c32f5c4260d6a85eef58564379f4bfb404
nivida <samuel@ethereum.org>
2020-10-21 16:58:18 +0200

    basic configured for jest

--- packages/govern/jest.config.js
+++ packages/govern/jest.config.js
@@ -1,4 +1,3 @@
-// TODO: Improve tests and add the branch property to the coverageThreshold config.
 const config = {
   // notifyMode: 'success-change',
   collectCoverage: true,
@@ -14,30 +13,11 @@ const config = {
   clearMocks: true,
   resetMocks: true,
   resetModules: true,
-  testMatch: ['/**/**Test.js'],
-  transform: {
-    '^.+\\.js$': '<rootDir>/../../jest.preprocessor.js'
-  },
+  testMatch: ['/**/**Test.ts'],
   bail: true,
   coveragePathIgnorePatterns: [
     'node_modules',
     'dist',
-  ]
-};
-
-/**
- * Returns the jest configuration with the moduleNameMapper object set.
- *
- * @param {Object} moduleNameMapper
- *
- * @returns {Object}
- */
-module.exports = (moduleNameMapper) => {
-  if (moduleNameMapper) {
-    config['moduleNameMapper'] = moduleNameMapper;
-
-    return config;
-  }
-
-  return config;
+  ],
+  preset: 'ts-jest'
 };
