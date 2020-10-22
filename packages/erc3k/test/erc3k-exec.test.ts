3fc3028099dd38e594df020e0bcab37521c24d61
Gabi <gabrielpk.18@gmail.com>
2020-10-22 16:17:15 -0300

    Core: move tests to typescript using typechain

--- packages/erc3k/test/erc3k-exec.test.ts
+++ packages/erc3k/test/erc3k-exec.test.ts
@@ -1,14 +1,14 @@
 import { ethers } from '@nomiclabs/buidler'
 import { expect } from 'chai'
-import { ERC3000Executor } from '../typechain'
+import { Erc3000ExecutorMock, Erc3000ExecutorMockFactory } from '../typechain'
 
 describe('ERC3000 Executor', function () {
-  let erc3kExec: ERC3000Executor
+  let erc3kExec: Erc3000ExecutorMock
 
   beforeEach(async () => {
-    const ERC3000Executor = await ethers.getContractFactory(
+    const ERC3000Executor = (await ethers.getContractFactory(
       'ERC3000ExecutorMock'
-    )
+    )) as Erc3000ExecutorMockFactory
     erc3kExec = await ERC3000Executor.deploy()
   })
 
--- packages/erc3k/test/erc3k.test.ts
+++ packages/erc3k/test/erc3k.test.ts
@@ -1,12 +1,14 @@
 import { ethers } from '@nomiclabs/buidler'
 import { expect } from 'chai'
-import { ERC3000 } from '../typechain'
+import { Erc3000Mock, Erc3000MockFactory } from '../typechain'
 
 describe('ERC3000', function () {
-  let erc3k: ERC3000
+  let erc3k: Erc3000Mock
 
   beforeEach(async () => {
-    const ERC3000 = await ethers.getContractFactory('ERC3000Mock')
+    const ERC3000 = (await ethers.getContractFactory(
+      'ERC3000Mock'
+    )) as Erc3000MockFactory
     erc3k = await ERC3000.deploy()
   })
 
