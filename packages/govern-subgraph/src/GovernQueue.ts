939b03cf9321aa27f5fe6cf11197e3053c48ba1d
Gabriel Garcia <gabrielpk.18@gmail.com>
2020-10-28 16:01:04 -0300

    Subgraph: rename to align with contracts (#86)

--- packages/govern-subgraph/src/GovernQueue.ts
+++ packages/govern-subgraph/src/GovernQueue.ts
@@ -1,4 +1,4 @@
-import { Address, BigInt, Bytes, ethereum } from '@graphprotocol/graph-ts'
+import { Address, Bytes } from '@graphprotocol/graph-ts'
 import {
   Challenged as ChallengedEvent,
   Configured as ConfiguredEvent,
@@ -15,169 +15,130 @@ import {
 } from '../generated/templates/GovernQueue/GovernQueue'
 import {
   Action as ActionEntity,
-  Config as ConfigEntity,
-  Challenge as ChallengeEntity,
   Collateral as CollateralEntity,
-  Evidence as EvidenceEntity,
-  QueuePacket as QueuePacketEntity,
-  GovernQueue as GovernQueueEntity,
-  ERC20 as ERC20Entity,
-  Schedule as ScheduleEntity,
-  Veto as VetoEntity
+  Config as ConfigEntity,
+  Container as ContainerEntity,
+  ContainerPayload as PayloadEntity,
+  GovernQueue as GovernQueueEntity
 } from '../generated/schema'
 import { frozenRoles, roleGranted, roleRevoked } from './lib/MiniACL'
-
-const NONE_STATUS = 'None'
-const APPROVED_STATUS = 'Approved'
-const CANCELLED_STATUS = 'Cancelled'
-const CHALLENGED_STATUS = 'Challenged'
-const EXECUTED_STATUS = 'Executed'
-const REJECTED_STATUS = 'Rejected'
-const SCHEDULED_STATUS = 'Scheduled'
-const VETOED_STATUS = 'Vetoed'
-
-const ALLOW_RULING = BigInt.fromI32(4)
+import { buildId, buildIndexedId } from './utils/ids'
+import {
+  APPROVED_STATUS,
+  CANCELLED_STATUS,
+  CHALLENGED_STATUS,
+  EXECUTED_STATUS,
+  NONE_STATUS,
+  REJECTED_STATUS,
+  SCHEDULED_STATUS,
+  ALLOW_RULING
+} from './utils/constants'
+import {
+  handleContainerEventChallenge,
+  handleContainerEventResolve,
+  handleContainerEventRule,
+  handleContainerEventSchedule,
+  handleContainerEventSubmitEvidence,
+  handleContainerEventVeto
+} from './utils/events'
 
 export function handleScheduled(event: ScheduledEvent): void {
-  const queue = loadOrCreateQueue(event.address)
-
-  const packet = loadOrCreateQueuePacket(event.params.containerHash, event)
-
+  let queue = loadOrCreateQueue(event.address)
+  let payload = loadOrCreatePayload(event.params.containerHash)
+  let container = loadOrCreateContainer(event.params.containerHash)
+  // Builds each of the actions bundled in the payload,
+  // and saves them to the DB.
   buildActions(event)
+  payload.nonce = event.params.payload.nonce
+  payload.executionTime = event.params.payload.executionTime
+  payload.submitter = event.params.payload.submitter
+  payload.executor = event.params.payload.executor.toHex()
+  payload.allowFailuresMap = event.params.payload.allowFailuresMap
+  payload.proof = event.params.payload.proof
 
-  packet.status = SCHEDULED_STATUS
-  packet.nonce = event.params.payload.nonce
-  packet.executionTime = event.params.payload.executionTime
-  packet.submitter = event.params.payload.submitter
-  packet.executor = event.params.payload.executor.toHexString()
-  packet.allowFailuresMap = event.params.payload.allowFailuresMap
-  packet.proof = event.params.payload.proof
+  container.payload = payload.id
+  container.state = SCHEDULED_STATUS
+  // This should always be possible, as a queue without a config
+  // should be impossible to get at this stage
+  container.config = queue.config
+  container.queue = queue.id
 
-  const schedule = new ScheduleEntity(event.params.containerHash.toHexString())
+  handleContainerEventSchedule(container, event)
 
-  const scheduleDeposit = loadOrCreateCollateral(event, '1')
-  scheduleDeposit.token = buildERC20(event.params.collateral.token)
-  scheduleDeposit.amount = event.params.collateral.amount
+  // add the container to the queue
+  let scheduled = queue.queued
+  scheduled.push(container.id)
+  queue.queued = scheduled
 
-  schedule.packet = packet.id
-  schedule.collateral = scheduleDeposit.id
-  schedule.createdAt = event.block.timestamp
-
-  // add the packet
-  const scheduled = queue.scheduled
-  scheduled.push(schedule.id)
-  queue.scheduled = scheduled
-
-  packet.save()
-  schedule.save()
+  payload.save()
+  container.save()
   queue.save()
 }
 
 export function handleExecuted(event: ExecutedEvent): void {
-  const packet = loadOrCreateQueuePacket(event.params.containerHash, event)
-
-  packet.status = EXECUTED_STATUS
-
-  packet.save()
+  let container = loadOrCreateContainer(event.params.containerHash)
+  container.state = EXECUTED_STATUS
+  container.save()
 }
 
 export function handleChallenged(event: ChallengedEvent): void {
-  const queue = loadOrCreateQueue(event.address)
+  let queue = loadOrCreateQueue(event.address)
+  let container = loadOrCreateContainer(event.params.containerHash)
 
-  const packet = loadOrCreateQueuePacket(event.params.containerHash, event)
+  container.state = CHALLENGED_STATUS
 
-  packet.status = CHALLENGED_STATUS
+  let containerEvent = handleContainerEventChallenge(container, event)
+  containerEvent.resolver = ConfigEntity.load(queue.config).resolver
 
-  const challenge = new ChallengeEntity(
-    event.params.containerHash.toHexString()
-  )
-
-  const challengeDeposit = loadOrCreateCollateral(event, '2')
-  challengeDeposit.token = buildERC20(event.params.collateral.token)
-  challengeDeposit.amount = event.params.collateral.amount
-
-  challenge.packet = packet.id
-  challenge.challenger = event.params.actor
-  challenge.arbitrator = loadOrCreateConfig(event.address).resolver
-  challenge.disputeId = event.params.resolverId
-  challenge.collateral = challengeDeposit.id
-  challenge.createdAt = event.block.timestamp
-
-  // add the challenged packet
-  const challenged = queue.challenged
-  challenged.push(challenge.id)
-  queue.challenged = challenged
-
-  packet.save()
-  challenge.save()
+  containerEvent.save()
+  container.save()
   queue.save()
 }
 
 export function handleResolved(event: ResolvedEvent): void {
-  const packet = loadOrCreateQueuePacket(event.params.containerHash, event)
-  const challenge = ChallengeEntity.load(
-    event.params.containerHash.toHexString()
-  )
+  let container = loadOrCreateContainer(event.params.containerHash)
 
-  packet.status = event.params.approved ? EXECUTED_STATUS : CANCELLED_STATUS
+  container.state = event.params.approved ? EXECUTED_STATUS : CANCELLED_STATUS
 
-  challenge.approved = event.params.approved
+  handleContainerEventResolve(container, event)
 
-  packet.save()
-  challenge.save()
+  container.save()
 }
 
 export function handleVetoed(event: VetoedEvent): void {
-  const queue = loadOrCreateQueue(event.address)
+  let queue = loadOrCreateQueue(event.address)
+  let container = loadOrCreateContainer(event.params.containerHash)
 
-  const packet = loadOrCreateQueuePacket(event.params.containerHash, event)
+  container.state = CANCELLED_STATUS
 
-  packet.status = VETOED_STATUS
+  handleContainerEventVeto(container, event)
 
-  const veto = new VetoEntity(event.params.containerHash.toHexString())
-
-  const vetoDeposit = loadOrCreateCollateral(event, '3')
-  vetoDeposit.token = buildERC20(event.params.collateral.token)
-  vetoDeposit.amount = event.params.collateral.amount
-
-  veto.packet = packet.id
-  veto.reason = event.params.reason
-  veto.submitter = event.params.actor
-  veto.collateral = vetoDeposit.id
-  veto.createdAt = event.block.timestamp
-
-  // add the veto packet
-  const vetoed = queue.vetoed
-  vetoed.push(veto.id)
-  queue.vetoed = vetoed
-
-  packet.save()
-  veto.save()
+  container.save()
   queue.save()
 }
 
 export function handleConfigured(event: ConfiguredEvent): void {
-  const queue = loadOrCreateQueue(event.address)
-  const config = loadOrCreateConfig(event.address)
+  let queue = loadOrCreateQueue(event.address)
 
-  const scheduleDeposit = loadOrCreateCollateral(event, '1')
-  scheduleDeposit.token = buildERC20(event.params.config.scheduleDeposit.token)
+  let configId = buildId(event)
+  let config = new ConfigEntity(configId)
+
+  let scheduleDeposit = new CollateralEntity(
+    buildIndexedId(event.transaction.hash.toHex(), 1)
+  )
+  scheduleDeposit.token = event.params.config.scheduleDeposit.token
   scheduleDeposit.amount = event.params.config.scheduleDeposit.amount
 
-  const challengeDeposit = loadOrCreateCollateral(event, '2')
-  challengeDeposit.token = buildERC20(
-    event.params.config.challengeDeposit.token
+  let challengeDeposit = new CollateralEntity(
+    buildIndexedId(event.transaction.hash.toHex(), 2)
   )
+  challengeDeposit.token = event.params.config.challengeDeposit.token
   challengeDeposit.amount = event.params.config.challengeDeposit.amount
 
-  const vetoDeposit = loadOrCreateCollateral(event, '3')
-  vetoDeposit.token = buildERC20(event.params.config.vetoDeposit.token)
-  vetoDeposit.amount = event.params.config.vetoDeposit.amount
-
+  config.queue = queue.id
   config.executionDelay = event.params.config.executionDelay
   config.scheduleDeposit = scheduleDeposit.id
   config.challengeDeposit = challengeDeposit.id
-  config.vetoDeposit = vetoDeposit.id
   config.resolver = event.params.config.resolver
   config.rules = event.params.config.rules
 
@@ -185,7 +146,6 @@ export function handleConfigured(event: ConfiguredEvent): void {
 
   scheduleDeposit.save()
   challengeDeposit.save()
-  vetoDeposit.save()
   config.save()
   queue.save()
 }
@@ -193,60 +153,49 @@ export function handleConfigured(event: ConfiguredEvent): void {
 // IArbitrable Events
 
 export function handleEvidenceSubmitted(event: EvidenceSubmittedEvent): void {
-  const governQueue = GovernQueueContract.bind(event.address)
-
-  const containerHash = governQueue.disputeItemCache(
+  let governQueue = GovernQueueContract.bind(event.address)
+  let containerHash = governQueue.disputeItemCache(
     event.params.arbitrator,
     event.params.disputeId
   )
+  let container = loadOrCreateContainer(containerHash)
 
-  const evidenceId = buildId(event)
-  const evidence = new EvidenceEntity(evidenceId)
-
-  evidence.challenge = containerHash.toHexString()
-  evidence.submitter = event.params.submitter
-  evidence.data = event.params.evidence
-  evidence.createdAt = event.block.timestamp
-
-  evidence.save()
+  handleContainerEventSubmitEvidence(container, event)
 }
 
 export function handleRuled(event: RuledEvent): void {
-  const governQueue = GovernQueueContract.bind(event.address)
-
-  const containerHash = governQueue.disputeItemCache(
+  let governQueue = GovernQueueContract.bind(event.address)
+  let containerHash = governQueue.disputeItemCache(
     event.params.arbitrator,
     event.params.disputeId
   )
+  let container = loadOrCreateContainer(containerHash)
 
-  const packet = loadOrCreateQueuePacket(containerHash, event)
-  const challenge = ChallengeEntity.load(containerHash.toHexString())
-
-  packet.status =
+  container.state =
     event.params.ruling === ALLOW_RULING ? APPROVED_STATUS : REJECTED_STATUS
-  challenge.ruling = event.params.ruling
 
-  packet.save()
-  challenge.save()
+  handleContainerEventRule(container, event)
+
+  container.save()
 }
 
 // MiniACL Events
 
 export function handleFrozen(event: FrozenEvent): void {
-  const queue = loadOrCreateQueue(event.address)
+  let queue = loadOrCreateQueue(event.address)
 
-  const roles = queue.roles!
+  let roles = queue.roles!
 
   frozenRoles(roles, event.params.role)
 }
 
 export function handleGranted(event: GrantedEvent): void {
-  const queue = loadOrCreateQueue(event.address)
+  let queue = loadOrCreateQueue(event.address)
 
-  const role = roleGranted(event.address, event.params.role, event.params.who)
+  let role = roleGranted(event.address, event.params.role, event.params.who)
 
   // add the role
-  const currentRoles = queue.roles
+  let currentRoles = queue.roles
   currentRoles.push(role.id)
   queue.roles = currentRoles
 
@@ -254,12 +203,12 @@ export function handleGranted(event: GrantedEvent): void {
 }
 
 export function handleRevoked(event: RevokedEvent): void {
-  const queue = loadOrCreateQueue(event.address)
+  let queue = loadOrCreateQueue(event.address)
 
-  const role = roleRevoked(event.address, event.params.role, event.params.who)
+  let role = roleRevoked(event.address, event.params.role, event.params.who)
 
   // add the role
-  const currentRoles = queue.roles
+  let currentRoles = queue.roles
   currentRoles.push(role.id)
   queue.roles = currentRoles
 
@@ -269,105 +218,51 @@ export function handleRevoked(event: RevokedEvent): void {
 // Helpers
 
 export function loadOrCreateQueue(entity: Address): GovernQueueEntity {
-  const queueId = entity.toHexString()
+  let queueId = entity.toHex()
   // Create queue
   let queue = GovernQueueEntity.load(queueId)
   if (queue === null) {
     queue = new GovernQueueEntity(queueId)
     queue.address = entity
-    queue.scheduled = []
-    queue.challenged = []
-    queue.vetoed = []
+    queue.config = ''
     queue.roles = []
   }
   return queue!
 }
 
-function loadOrCreateConfig(entity: Address): ConfigEntity {
-  const configId = entity.toHexString()
-  // Create config
-  let config = ConfigEntity.load(configId)
-  if (config === null) {
-    config = new ConfigEntity(configId)
-    config.queue = entity.toHexString()
+export function loadOrCreateContainer(containerHash: Bytes): ContainerEntity {
+  let ContainerId = containerHash.toHex()
+  // Create container
+  let container = ContainerEntity.load(ContainerId)
+  if (container === null) {
+    container = new ContainerEntity(ContainerId)
+    container.state = NONE_STATUS
+    container.history = []
   }
-  return config!
+  return container!
 }
 
-function loadOrCreateQueuePacket(
-  containerHash: Bytes,
-  event: ethereum.Event
-): QueuePacketEntity {
-  const PacketId = containerHash.toHexString()
-  // Create packet
-  let packet = QueuePacketEntity.load(PacketId)
-  if (packet === null) {
-    packet = new QueuePacketEntity(PacketId)
-    packet.queue = event.address.toHexString()
-    packet.status = NONE_STATUS
+function loadOrCreatePayload(containerHash: Bytes): PayloadEntity {
+  let PayloadId = containerHash.toHex()
+  // Create payload
+  let payload = PayloadEntity.load(PayloadId)
+  if (payload === null) {
+    payload = new PayloadEntity(PayloadId)
   }
-  return packet!
-}
-
-function loadOrCreateCollateral(
-  event: ethereum.Event,
-  index: String
-): CollateralEntity {
-  const collateralId = event.transaction.hash.toHexString() + index
-  // Create collateral
-  let collateral = CollateralEntity.load(collateralId)
-  if (collateral === null) {
-    collateral = new CollateralEntity(collateralId)
-  }
-  return collateral!
-}
-
-function loadOrCreateChallenge(
-  containerHash: Bytes,
-  event: ethereum.Event
-): ChallengeEntity {
-  const challengeId = containerHash.toHexString()
-  // Create challenge
-  let challenge = ChallengeEntity.load(challengeId)
-  if (challenge === null) {
-    challenge = new ChallengeEntity(challengeId)
-    challenge.createdAt = event.block.timestamp
-  }
-  return challenge!
-}
-
-function buildId(event: ethereum.Event): string {
-  return event.transaction.hash.toHexString() + event.logIndex.toString()
-}
-
-function buildActionId(containerHash: Bytes, index: number): string {
-  return containerHash.toHexString() + index.toString()
+  return payload!
 }
 
 function buildActions(event: ScheduledEvent): void {
-  const actions = event.params.payload.actions
-  for (let id = 0; id < actions.length; id++) {
-    const actionId = buildActionId(event.params.containerHash, id)
-    const action = new ActionEntity(actionId)
+  let actions = event.params.payload.actions
+  for (let index = 0; index < actions.length; index++) {
+    let actionId = buildIndexedId(event.params.containerHash.toHex(), index)
+    let action = new ActionEntity(actionId)
 
-    action.to = actions[id].to
-    action.value = actions[id].value
-    action.data = actions[id].data
-    action.packet = event.params.containerHash.toHexString()
+    action.to = actions[index].to
+    action.value = actions[index].value
+    action.data = actions[index].data
+    action.payload = event.params.containerHash.toHex()
 
     action.save()
   }
-}
-
-export function buildERC20(address: Address): string {
-  const id = address.toHexString()
-  let token = ERC20Entity.load(id)
-
-  if (token === null) {
-    token = new ERC20Entity(id)
-    token.address = address
-    token.save()
-  }
-
-  return token.id
 }
