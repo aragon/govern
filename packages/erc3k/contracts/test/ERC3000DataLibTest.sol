8f289afe3f6fcfc3e11b71515bcef650d8a2e8a1
nivida <samuel@ethereum.org>
2020-10-28 12:00:46 +0100

    simple contract created to test the correct hashing of the off-chain data

---
+++ packages/erc3k/contracts/test/ERC3000DataLibTest.sol
@@ -0,0 +1,24 @@
+/*
+ * SPDX-License-Identifier:    GPL-3.0
+ */
+
+pragma solidity 0.6.8;
+pragma experimental ABIEncoderV2;
+
+import "../ERC3000Data.sol";
+
+contract ERC3000DataLibTest {
+    using ERC3000Data for *;
+
+    function testConfigHash(ERC3000Data.Config memory _initialConfig) public returns (bytes32) {
+        return _initialConfig.hash();
+    }
+
+    function testContainerHash(ERC3000Data.Container memory _container) public returns (bytes32) {
+        return _container.hash();
+    }
+
+    function testPayloadHash(ERC3000Data.Payload memory _payload) public returns (bytes32) {
+        return _payload.hash();
+    }
+}
