facb077641edd32144fb1fde9f762cdce77e3b22
nivida <samuel@ethereum.org>
2020-10-29 11:25:37 +0100

    IERCRegistry updated

--- packages/erc3k/contracts/IERC3000Registry.sol
+++ packages/erc3k/contracts/IERC3000Registry.sol
@@ -7,7 +7,7 @@ pragma solidity 0.6.8;
 import "./IERC3000.sol";
 import "./IERC3000Executor.sol";
 
-interface IERC3000Registry is ERC3000Interface {
+abstract contract IERC3000Registry is ERC3000Interface {
     /**
      * @notice Registers a IERC3000Executor and IERC3000 contract by a name and with his metadata
      * @param executor IERC3000Executor contract
@@ -15,13 +15,13 @@ interface IERC3000Registry is ERC3000Interface {
      * @param name The name of this DAO
      * @param initialMetadata Additional data to store for this DAO
      */
-    function register(IERC3000Executor executor, IERC3000 queue, string calldata name, bytes calldata initialMetadata) external;
+    function register(IERC3000Executor executor, IERC3000 queue, string calldata name, bytes calldata initialMetadata) virtual external;
     event Registered(IERC3000Executor indexed executor, IERC3000 queue, address indexed registrant, string name);
 
     /**
      * @notice Sets or updates the metadata of a DAO
      * @param metadata Additional data to store for this DAO
      */
-    function setMetadata(bytes memory metadata) external;
+    function setMetadata(bytes memory metadata) virtual public;
     event SetMetadata(IERC3000Executor indexed executor, bytes metadata);
 }
