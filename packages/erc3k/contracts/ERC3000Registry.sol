5424a8a711f9aecd046c48b506b3de7fb6fc51b7
nivida <samuel@ethereum.org>
2020-10-29 07:54:05 +0100

    renamed 'dao' to 'executor' in ERC3000Registry

--- packages/erc3k/contracts/ERC3000Registry.sol
+++ packages/erc3k/contracts/ERC3000Registry.sol
@@ -10,18 +10,18 @@ import "./ERC3000Executor.sol";
 abstract contract ERC3000Registry is ERC3000Interface {
     /**
      * @notice Registers a ERC3000Executor and ERC3000 contract by a name and with his metadata
-     * @param dao ERC3000Executor contract
+     * @param executor ERC3000Executor contract
      * @param queue ERC3000 contract
      * @param name The name of this DAO
      * @param initialMetadata Additional data to store for this DAO
      */
-    function register(ERC3000Executor dao, ERC3000 queue, string calldata name, bytes calldata initialMetadata) virtual external;
-    event Registered(ERC3000Executor indexed dao, ERC3000 queue, address indexed registrant, string name);
+    function register(ERC3000Executor executor, ERC3000 queue, string calldata name, bytes calldata initialMetadata) virtual external;
+    event Registered(ERC3000Executor indexed executor, ERC3000 queue, address indexed registrant, string name);
 
     /**
      * @notice Sets or updates the metadata of a DAO
      * @param metadata Additional data to store for this DAO
      */
     function setMetadata(bytes memory metadata) virtual public;
-    event SetMetadata(ERC3000Executor indexed dao, bytes metadata);
+    event SetMetadata(ERC3000Executor indexed executor, bytes metadata);
 }
