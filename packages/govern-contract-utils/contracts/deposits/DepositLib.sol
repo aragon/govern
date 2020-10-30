a901ffd3c364c7a75b1f5ebdf83412145990acaa
Oliver <onbjerg@users.noreply.github.com>
2020-10-30 12:24:02 +0100

    Rename events in DepositLib to `Lock`/`Unlock` (#135)

--- packages/govern-contract-utils/contracts/deposits/DepositLib.sol
+++ packages/govern-contract-utils/contracts/deposits/DepositLib.sol
@@ -11,15 +11,15 @@ import "../erc20/SafeERC20.sol";
 library DepositLib {
     using SafeERC20 for ERC20;
 
-    event Collected(address indexed token, address indexed from, uint256 amount);
-    event Released(address indexed token, address indexed to, uint256 amount);
+    event Lock(address indexed token, address indexed from, uint256 amount);
+    event Unlock(address indexed token, address indexed to, uint256 amount);
 
     function collectFrom(ERC3000Data.Collateral memory _collateral, address _from) internal {
         if (_collateral.amount > 0) {
             ERC20 token = ERC20(_collateral.token);
             require(token.safeTransferFrom(_from, msg.sender, _collateral.amount), "queue: bad get token");
 
-            emit Collected(_collateral.token, _from, _collateral.amount);
+            emit Lock(_collateral.token, _from, _collateral.amount);
         }
     }
 
@@ -28,7 +28,7 @@ library DepositLib {
             ERC20 token = ERC20(_collateral.token);
             require(token.safeTransfer(_to, _collateral.amount), "queue: bad send token");
 
-            emit Released(_collateral.token, _to, _collateral.amount);
+            emit Unlock(_collateral.token, _to, _collateral.amount);
         }
     }
 }
