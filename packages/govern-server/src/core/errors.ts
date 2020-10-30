17be272158bfd8a8a95ba8a0aa96c80e171bbe4a
Pierre Bertet <hello@pierre.world>
2020-10-30 02:54:45 +0000

    Server: remove unused errors

--- packages/govern-server/src/core/errors.ts
+++ packages/govern-server/src/core/errors.ts
@@ -69,69 +69,12 @@ export class ErrorUnexpectedResult extends ErrorException {
   }
 }
 
-export class ErrorInvalidEthereum extends ErrorInvalid {
-  constructor(
-    message = 'The Ethereum provider doesn’t seem to be valid.',
-    {
-      code = 'ErrorInvalidEthereum',
-      name = 'ErrorInvalidEthereum',
-    }: ErrorOptions = {}
-  ) {
-    super(message, { code, name })
-  }
-}
-
-export class ErrorInvalidLocation extends ErrorInvalid {
-  constructor(
-    message = 'The Ethereum address or ENS domain doesn’t seem to be valid.',
-    {
-      code = 'ErrorInvalidLocation',
-      name = 'ErrorInvalidLocation',
-    }: ErrorOptions = {}
-  ) {
-    super(message, { code, name })
-  }
-}
-
 export class ErrorInvalidNetwork extends ErrorInvalid {
   constructor(
     message = 'The network doesn’t seem to be valid.',
     {
       code = 'ErrorInvalidNetwork',
       name = 'ErrorInvalidNetwork',
-    }: ErrorOptions = {}
-  ) {
-    super(message, { code, name })
-  }
-}
-
-export class ErrorInvalidConnector extends ErrorInvalid {
-  constructor(
-    message = 'The connector doesn’t seem to be valid.',
-    {
-      code = 'ErrorInvalidConnector',
-      name = 'ErrorInvalidConnector',
-    }: ErrorOptions = {}
-  ) {
-    super(message, { code, name })
-  }
-}
-
-export class ErrorInvalidApp extends ErrorInvalid {
-  constructor(
-    message = 'The value doesn’t seem to be an app.',
-    { code = 'ErrorInvalidApp', name = 'ErrorInvalidApp' }: ErrorOptions = {}
-  ) {
-    super(message, { code, name })
-  }
-}
-
-export class ErrorUnsufficientBalance extends ErrorException {
-  constructor(
-    message = 'Unsufficient balance on the account.',
-    {
-      code = 'ErrorUnsufficientBalance',
-      name = 'ErrorUnsufficientBalance',
     }: ErrorOptions = {}
   ) {
     super(message, { code, name })
