86c57a3a74e875eda0fbb1afd81f196677aabbab
nivida <samuel@ethereum.org>
2020-11-01 13:17:08 +0100

    unit tests updated

--- packages/govern/tests/internal/actions/DaoActionTest.ts
+++ packages/govern/tests/internal/actions/DaoActionTest.ts
@@ -1,10 +1,35 @@
+import GraphQLClient from '../../../internal/clients/graphql/GraphQLClient'
 import DaoAction from '../../../internal/actions/DaoAction'
+import dao from '../../../internal/clients/graphql/queries/dao'
+
+//Mocks
+jest.mock('../../../internal/clients/graphql/GraphQLClient')
 
 /**
  * DaoAction test
  */
 describe('DaoActionTest', () => {
-  it('calls the constructor and initiates the class as expected', () => {
-    new DaoAction({ name: 'myName' })
+  let action: DaoAction,
+    clientMockClass = GraphQLClient as jest.MockedClass<typeof GraphQLClient>,
+    clientMock: GraphQLClient
+
+
+  beforeEach(() => {
+    action = new DaoAction({ name: 'myName' })
+    clientMock = clientMockClass.mock.instances[0]
+  })
+
+  it('calls execute and does map the response accordingly', async () => {
+    clientMock.request = jest.fn((query: string, args: any = {}) => {
+      expect(query).toEqual(dao);
+
+      expect(args).toEqual({name: 'myName'})
+
+      return Promise.resolve({dao: true})
+    });
+
+    const response = await action.execute();
+
+    expect(response).toEqual(true)
   })
 })
