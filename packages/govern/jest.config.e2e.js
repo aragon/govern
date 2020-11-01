6e23bd26188e2697248e39fd2a68e5327a959e3f
nivida <samuel@ethereum.org>
2020-11-01 11:32:15 +0100

    basic setup e2e run

---
+++ packages/govern/jest.config.e2e.js
@@ -0,0 +1,4 @@
+const jestConfig = require('./jest.config.js')
+jestConfig.testMatch = ['/**/**Test.e2e.ts']
+
+module.exports = jestConfig;
---
+++ packages/govern/scripts/start.e2e.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Go to govern server package and start dev server
+cd ../govern-server/
+yarn dev
+
+# Go to govern package and start e2e tests
+cd ../govern
+jest -c ./jest.config.e2e.js
