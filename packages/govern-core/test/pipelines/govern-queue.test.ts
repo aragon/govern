00f86ebf8db13629cc931f3267128b54e8e525d9
nivida <samuel@ethereum.org>
2020-11-02 13:43:59 +0100

    templates test cases for govern-queue.test created to later check if anything is covered

--- packages/govern-core/test/pipelines/govern-queue.test.ts
+++ packages/govern-core/test/pipelines/govern-queue.test.ts
@@ -1,136 +1,512 @@
 import { ethers } from '@nomiclabs/buidler'
 import { expect } from 'chai'
 import { Signer } from 'ethers'
-import { GovernQueue, GovernQueueFactory } from '../../typechain'
+import {
+  GovernQueue,
+  GovernQueueFactory,
+  ArbitratorMock,
+  ArbitratorMockFactory,
+  ArbitratorBadFeePullMock,
+  ArbitratorBadFeePullMockFactory,
+  ArbitratorBadApproveMock,
+  ArbitratorBadApproveMockFactory,
+  ArbitratorBadResetMock,
+  ArbitratorBadResetMockFactory,
+  ArbitratorRejectMock,
+  ArbitratorRejectMockFactory,
+  ArbitratorNoStateChangeOnRejectMock,
+  ArbitratorNoStateChangeOnRejectMockFactory,
+  ArbitratorCallsRuleMock,
+  ArbitratorCallsRuleMockFactory
+} from '../../typechain'
+import * as container from './container.json'
+import { getConfigHash, getContainerHash } from './helpers'
+import { formatBytes32String } from 'ethers/lib/utils'
 
-const ZERO_ADDR = '0x0000000000000000000000000000000000000000'
-
+// TODO: Create mock contract to check the return values of public methods
 describe('Govern Queue', function() {
-  let accounts: Signer[], owner: Signer, ownerAddr
-  let gq: GovernQueue
+  let accounts: Signer[],
+    owner: Signer,
+    ownerAddr: string,
+    chainId: number,
+    gq: GovernQueue
 
-  const INIT_CONFIG = {
-    executionDelay: 0,
-    scheduleDeposit: {
-      token: ZERO_ADDR,
-      amount: 0
-    },
-    challengeDeposit: {
-      token: ZERO_ADDR,
-      amount: 0
-    },
-    vetoDeposit: {
-      token: ZERO_ADDR,
-      amount: 0
-    },
-    resolver: ZERO_ADDR,
-    rules: '0x'
+  const EVENTS = {
+    SCHEDULED: 'Scheduled',
+    LOCK: 'Lock',
+    UNLOCK: 'Unlock',
+    EXECUTED: 'Executed',
+    CHALLENGED: 'Challenged',
+    EVIDENCE_SUBMITTED: 'EvidenceSubmitted',
+    VETOED: 'Vetoed',
+    CONFIGURED: 'Configured',
+    RESOLVED: 'Resolved',
+    RULED: 'Ruled'
+  }
+
+  const ERRORS = {
+    BAD_NONCE: 'queue: bad nonce',
+    BAD_CONFIG: 'queue: bad config',
+    BAD_DELAY: 'queue: bad delay',
+    BAD_SUBMITTER: 'queue: bad submitter',
+    WAIT_MORE: 'queue: wait more',
+    BAD_FEE_PULL: 'queue: bad fee pull',
+    BAD_APPROVE: 'queue: bad approve',
+    BAD_RESET: 'queue: bad reset',
+    UNRESOLVED: 'queue: unresolved',
+    EVIDENCE: 'queue: evidence'
   }
 
   beforeEach(async () => {
     accounts = await ethers.getSigners()
     owner = accounts[0]
     ownerAddr = await owner.getAddress()
+    chainId = (await ethers.provider.getNetwork()).chainId
 
     const GQ = (await ethers.getContractFactory(
       'GovernQueue'
     )) as GovernQueueFactory
-    gq = (await GQ.deploy(ownerAddr, INIT_CONFIG)) as GovernQueue
+    gq = (await GQ.deploy(ownerAddr, container.config)) as GovernQueue
   })
 
   context('GovernQueue.schedule', () => {
-    it('returns with the expected result', () => {
+    it('returns with the expected result', async () => {
+      const containerHash = getContainerHash(container, ownerAddr, chainId)
 
+      await expect(gq.schedule(container))
+        .to.emit(gq, EVENTS.LOCK).withArgs(
+          container.config.scheduleDeposit.token,
+          ownerAddr,
+          container.config.scheduleDeposit.amount
+        )
+        .to.emit(gq, EVENTS.SCHEDULED).withArgs(
+          containerHash,
+          container.payload,
+          container.config.scheduleDeposit
+        )
+
+      expect(await gq.queue(containerHash)).to.equal({ state: 'Scheduled' })
     })
 
-    it('errors with "queue: bad nonce"', () => {
+    it('reverts with "queue: bad nonce"', async () => {
+      container.payload.nonce = 0
 
+      await expect(gq.schedule(container)).to.be.revertedWith(ERRORS.BAD_NONCE)
     })
 
-    it('errors with "queue: bad config"', () => {
+    it('reverts with "queue: bad config"', async () => {
+      container.config.executionDelay = 0
 
+      await expect(gq.schedule(container)).to.be.revertedWith(ERRORS.BAD_CONFIG)
     })
 
-    it('errors with "queue: bad delay"', () => {
+    it('reverts with "queue: bad delay"', async () => {
+      container.config.executionDelay = 0
 
+      await expect(gq.schedule(container)).to.be.revertedWith(ERRORS.BAD_DELAY)
     })
 
-    it('errors with "queue: bad submitter"', () => {
+    it('reverts with "queue: bad submitter"', async () => {
+      container.payload.submitter = ownerAddr
 
+      await expect(gq.schedule(container)).to.be.revertedWith(ERRORS.BAD_SUBMITTER)
     })
   })
 
   context('GovernQueue.execute', () => {
-    it('returns with the expected result', () => {
+    it('returns with the expected result', async () => {
+      const containerHash = getContainerHash(container, ownerAddr, chainId)
 
+      await expect(gq.execute(container))
+        .to.emit(gq, EVENTS.UNLOCK).withArgs(
+          container.config.scheduleDeposit.token,
+          ownerAddr,
+          container.config.scheduleDeposit.amount
+        ).to.emit(gq, EVENTS.EXECUTED).withArgs(
+          containerHash,
+          ownerAddr
+        )
+
+      expect(await gq.queue(containerHash)).to.equal({ state: 'Executed' })
     })
 
-    it('errors with "queue: wait more"', () => {
+    it('reverts with "queue: wait more"', async () => {
+      container.payload.executionTime = 0
 
+      await expect(gq.schedule(container)).to.be.revertedWith(ERRORS.WAIT_MORE)
     })
   })
 
   context('GovernQueue.challenge', () => {
-    it('returns with the expected result', () => {
+    it('returns with the expected result', async () => {
+      const ArbitratorMock = (await ethers.getContractFactory(
+        'ArbitratorMock'
+      )) as ArbitratorMockFactory
+      const arbitratorMock = await ArbitratorMock.deploy()
 
+      container.config.resolver = arbitratorMock.address
+      const containerHash = getContainerHash(container, ownerAddr, chainId)
+      const reasonAsBytes32 = formatBytes32String('NOPE!')
+      const disputeId = ''
+
+      await expect(gq.challenge(container, reasonAsBytes32))
+        .to.emit(gq, EVENTS.LOCK).withArgs(
+          container.config.challengeDeposit.token,
+          ownerAddr,
+          container.config.challengeDeposit.amount
+        ).to.emit(gq, EVENTS.CHALLENGED).withArgs(
+          containerHash,
+          ownerAddr,
+          reasonAsBytes32,
+          disputeId,
+          container.config.challengeDeposit
+        ).to.emit(gq, EVENTS.EVIDENCE_SUBMITTED).withArgs(
+          container.config.resolver,
+          disputeId,
+          container.payload.submitter,
+          container.payload.proof,
+          true
+        ).to.emit(gq, EVENTS.EVIDENCE_SUBMITTED).withArgs(
+          container.config.resolver,
+          disputeId,
+          ownerAddr,
+          reasonAsBytes32,
+          true
+        )
+
+      expect(await gq.queue(containerHash))
+        .to.equal({ state: 'Challenged' })
+
+      expect(await gq.disputeItemCache(container.config.resolver, disputeId))
+        .to.equal(containerHash)
     })
 
-    it('errors with "queue: bad fee pull"', () => {
+    it('reverts with "queue: bad fee pull"', async () => {
+      const ArbitratorBadFeePullMock = (await ethers.getContractFactory(
+        'ArbitratorBadFeePullMock'
+      )) as ArbitratorBadFeePullMockFactory
+      const arbitratorBadFeePullMock = await ArbitratorBadFeePullMock.deploy()
 
+      container.config.resolver = arbitratorBadFeePullMock.address
+
+      await expect(gq.challenge(container, formatBytes32String('NOPE!')))
+        .to.be.revertedWith(ERRORS.BAD_FEE_PULL)
     })
 
-    it('errors with "queue: bad approve"', () => {
+    it('reverts with "queue: bad approve"', async () => {
+      const ArbitratorBadApproveMock = (await ethers.getContractFactory(
+        'ArbitratorBadApproveMock'
+      )) as ArbitratorBadApproveMock
+      const arbitratorBadApproveMock = await ArbitratorBadApproveMock.deploy()
 
+      container.config.resolver = arbitratorBadApproveMock.address
+
+      await expect(gq.challenge(container, formatBytes32String('NOPE!')))
+        .to.be.revertedWith(ERRORS.BAD_APPROVE)
     })
 
-    it('errors with "queue: bad reset"', () => {
+    it('reverts with "queue: bad reset"', async () => {
+      const ArbitratorBadResetMock = (await ethers.getContractFactory(
+        'ArbitratorBadResetMock'
+      )) as ArbitratorBadResetMock
+      const arbitratorBadResetMock = await ArbitratorBadResetMock.deploy()
 
+      container.config.resolver = arbitratorBadResetMock.address
+
+      await expect(gq.challenge(container, formatBytes32String('NOPE!')))
+        .to.be.revertedWith(ERRORS.BAD_RESET)
     })
   })
 
   context('GovernQueue.resolve', () => {
-    it('returns with the expected result', () => {
+    it('emits resolved with approved true', async () => {
+      // TODO: update & deepCopy container
+      await gq.schedule(container)
 
+      const containerHash = getContainerHash(container, ownerAddr, chainId)
+
+      // TODO: Generate disputeId
+      const disputeId = ''
+
+      await expect(gq.resolve(container, disputeId))
+        .to.emit(gq, EVENTS.UNLOCK).withArgs(
+          container.config.scheduleDeposit.token,
+          ownerAddr,
+          container.config.scheduleDeposit.amount
+        ).to.emit(gq, EVENTS.RESOLVED).withArgs(
+          containerHash,
+          ownerAddr,
+          true
+        ).to.emit(gq, EVENTS.EXECUTED).withArgs(
+          containerHash,
+          ownerAddr
+        )
+
+      expect(await gq.queue(containerHash)).to.equal({ state: 'Executed' })
     })
 
-    it('errors with "queue: unresolved"', () => {
+    it('emits resolved with approved false', async () => {
+      // TODO: update & deepCopy container
+      await gq.schedule(container)
 
+      const containerHash = getContainerHash(container, ownerAddr, chainId)
+
+      // TODO: Generate disputeId
+      const disputeId = ''
+
+      await expect(gq.resolve(container, disputeId))
+        .to.emit(gq, EVENTS.UNLOCK).withArgs(
+          container.config.scheduleDeposit.token,
+          ownerAddr,
+          container.config.scheduleDeposit.amount
+        ).to.emit(gq, EVENTS.RESOLVED).withArgs(
+          containerHash,
+          ownerAddr,
+          true
+        ).to.emit(gq, EVENTS.EXECUTED).withArgs(
+          containerHash,
+          ownerAddr
+        )
+
+      expect(await gq.queue(containerHash)).to.equal({ state: 'Executed' })
+    })
+
+    it('executes the arbitrator ruling and emits the expected events', async () => {
+      const ArbitratorMock = (await ethers.getContractFactory(
+        'ArbitratorMock'
+      )) as ArbitratorMockFactory
+      const arbitratorMock = await ArbitratorMock.deploy()
+
+      // TODO: update & deepCopy container
+      await gq.schedule(container)
+      await gq.challenge(container, formatBytes32String('NOPE!'))
+
+      container.config.resolver = arbitratorMock.address
+      const containerHash = getContainerHash(container, ownerAddr, chainId)
+
+      // TODO: Generate disputeId
+      const disputeId = ''
+
+      await expect(gq.resolve(container, disputeId))
+        .to.emit(gq, EVENTS.UNLOCK).withArgs(
+          container.config.scheduleDeposit.token,
+          ownerAddr,
+          container.config.scheduleDeposit.amount
+        ).to.emit(gq, EVENTS.RESOLVED).withArgs(
+          containerHash,
+          ownerAddr
+        ).to.emit(gq, EVENTS.EXECUTED).withArgs(
+          containerHash,
+          ownerAddr
+        )
+
+      expect(await gq.queue(containerHash)).to.equal({ state: 'Executed' })
+    })
+
+    it('emits resolved with approved false and rejects', async () => {
+      const ArbitratorRejectMock = (await ethers.getContractFactory(
+        'ArbitratorRejectMock'
+      )) as ArbitratorRejectMockFactory
+      const arbitratorRejectMock = await ArbitratorRejectMock.deploy()
+
+      container.config.resolver = arbitratorRejectMock.address
+
+      // TODO: update & deepCopy container
+      await gq.schedule(container)
+      await gq.challenge(container, formatBytes32String('NOPE!'))
+
+      const containerHash = getContainerHash(container, ownerAddr, chainId)
+
+      // TODO: Generate disputeId
+      const disputeId = ''
+
+      await expect(gq.resolve(container, disputeId))
+        .to.emit(gq, EVENTS.UNLOCK).withArgs(
+          container.config.scheduleDeposit.token,
+          ownerAddr,
+          container.config.scheduleDeposit.amount
+        ).to.emit(gq, EVENTS.UNLOCK).withArgs(
+          container.config.challengeDeposit.token,
+          ownerAddr,
+          container.config.challengeDeposit.amount
+        ).to.emit(gq, EVENTS.RESOLVED).withArgs(
+          containerHash,
+          ownerAddr,
+          false
+        )
+
+      // TODO: Check if event is actually missing with Jorge
+      // .to.emit(gq, EVENTS.REJECTED).withArgs(
+      //   containerHash,
+      //   ownerAddr
+      // )
+
+      expect(await gq.challengerCache(containerHash))
+        .to.equal('0x0000000000000000000000000000000000000000')
+
+      expect(await gq.queue(containerHash))
+        .to.equal({ state: 'Cancelled' })
+    })
+
+    it('reverts with "queue: unresolved"', async () => {
+      const ArbitratorNoStateChangeOnRejectMock = (await ethers.getContractFactory(
+        'ArbitratorNoStateChangeOnRejectMock'
+      )) as ArbitratorNoStateChangeOnRejectMockFactory
+      const arbitratorNoStateChangeOnRejectMock = await ArbitratorNoStateChangeOnRejectMock.deploy()
+
+      container.config.resolver = arbitratorNoStateChangeOnRejectMock.address
+
+      // TODO: update & deepCopy container
+      await gq.schedule(container)
+      await gq.challenge(container, formatBytes32String('NOPE!'))
+
+      // TODO: Generate disputeId
+      const disputeId = ''
+
+      await expect(gq.resolve(container, disputeId))
+        .to.be.revertedWith(ERRORS.UNRESOLVED)
     })
   })
 
   context('GovernQueue.veto', () => {
-    it('runs as expected', () => {
+    it('runs as expected', async () => {
+      const containerHash = getContainerHash(container, ownerAddr, chainId)
+      const reasonAsBytes32 = formatBytes32String('NOPE!')
 
+      await expect(gq.veto(containerHash, reasonAsBytes32))
+        .to.emit(gq, EVENTS.VETOED).withArgs(
+          containerHash,
+          ownerAddr,
+          reasonAsBytes32
+        )
+
+      expect(await gq.queue(containerHash)).to.equal({ state: 'Cancelled' })
     })
   })
 
   context('GovernQueue.configure', () => {
-    it('returns with the expected result', () => {
+    it('updated the configuration as expected', async () => {
+      const configHash = getConfigHash(container)
 
+      await expect(gq.configure(container.config))
+        .to.emit(gq, EVENTS.CONFIGURED).withArgs(
+          configHash,
+          ownerAddr,
+          container.config
+        )
+
+      expect(await gq.configHash()).to.equal(configHash)
     })
   })
 
   context('GovernQueue.executeApprove', () => {
-    it('returns with the expected result', () => {
+    it('emits the expected events', async () => {
+      const containerHash = getContainerHash(container, ownerAddr, chainId)
 
+      await expect(gq.executeApproved(container))
+        .to.emit(gq, EVENTS.UNLOCK).withArgs(
+          container.config.scheduleDeposit.token,
+          ownerAddr,
+          container.config.scheduleDeposit.amount
+        ).to.emit(gq, EVENTS.UNLOCK).withArgs(
+          container.config.challengeDeposit.token,
+          ownerAddr,
+          container.config.challengeDeposit.amount
+        ).to.emit(gq, EVENTS.EXECUTED).withArgs(
+          containerHash,
+          ownerAddr
+        )
+
+      expect(await gq.queue(containerHash))
+        .to.equal({ state: 'Executed' })
+
+      expect(await gq.challengerCache(containerHash))
+        .to.equal('0x0000000000000000000000000000000000000000')
     })
   })
 
   context('GovernQueue.settleRejection', () => {
-    it('returns with the expected result', () => {
+    it('clears anything as expected', async () => {
+      await expect(gq.settleRejection(container))
+        .to.emit(gq, EVENTS.UNLOCK).withArgs(
+          container.config.scheduleDeposit.token,
+          ownerAddr,
+          container.config.scheduleDeposit.amount
+        ).to.emit(gq, EVENTS.UNLOCK).withArgs(
+          container.config.challengeDeposit.token,
+          ownerAddr,
+          container.config.challengeDeposit.amount
+        )
 
+      expect(await gq.challengerCache(getContainerHash(container, ownerAddr, chainId)))
+        .to.equal('0x0000000000000000000000000000000000000000')
     })
   })
 
   context('GovernQueue.rule', () => {
-    it('returns with the expected result', () => {
+    it('sets the state to "Approved" and clears the disputeItemCache', async () => {
+      const ArbitratorCallsRuleMock = (await ethers.getContractFactory(
+        'ArbitratorCallsRuleMock'
+      )) as ArbitratorCallsRuleMockFactory
+      const arbitratorCallsRuleMock = await ArbitratorCallsRuleMock.deploy()
 
+      container.config.resolver = arbitratorCallsRuleMock.address
+
+      // TODO: update & deepCopy container
+      await gq.schedule(container)
+      await gq.challenge(container, formatBytes32String('NOPE!'))
+
+      // TODO: Generate disputeId
+      const disputeId = ''
+
+      await expect(arbitratorCallsRuleMock.execRule(disputeId, 4))
+        .to.emit(gq, EVENTS.RULED).withArgs(
+          arbitratorCallsRuleMock.address,
+          disputeId,
+          4
+        )
+
+      expect(await gq.queue(getContainerHash(container, ownerAddr, chainId)))
+        .to.equal({state: 'Approved'})
+
+      expect(await gq.disputeItemCache(container.config.resolver, disputeId))
+        .to.equal('0x0000000000000000000000000000000000000000')
+    })
+
+    it('sets the state to "Rejected" and clears the disputeItemCache', async () => {
+      const ArbitratorCallsRuleMock = (await ethers.getContractFactory(
+        'ArbitratorCallsRuleMock'
+      )) as ArbitratorCallsRuleMockFactory
+      const arbitratorCallsRuleMock = await ArbitratorCallsRuleMock.deploy()
+
+      container.config.resolver = arbitratorCallsRuleMock.address
+
+      // TODO: update & deepCopy container
+      await gq.schedule(container)
+      await gq.challenge(container, formatBytes32String('NOPE!'))
+
+      // TODO: Generate disputeId
+      const disputeId = ''
+
+      await expect(arbitratorCallsRuleMock.execRule(disputeId, 0))
+        .to.emit(gq, EVENTS.RULED).withArgs(
+          arbitratorCallsRuleMock.address,
+          disputeId,
+          0
+        )
+
+      expect(await gq.queue(getContainerHash(container, ownerAddr, chainId)))
+        .to.equal({state: 'Rejected'})
+
+      expect(await gq.disputeItemCache(container.config.resolver, disputeId))
+        .to.equal('0x0000000000000000000000000000000000000000')
     })
   })
 
   context('GovernQueue.submitEvidence', () => {
-    it('reverts as expected', () => {
-
+    it('reverts as expected', async () => {
+      await expect(gq.submitEvidence(100, '0x00', false))
+        .to.be.revertedWith(ERRORS.EVIDENCE)
     })
   })
 
---
+++ packages/govern-core/test/pipelines/helpers.ts
@@ -0,0 +1,86 @@
+import { keccak256, defaultAbiCoder, solidityPack } from 'ethers/lib/utils'
+
+/**
+ * Returns the hash of the container payload
+ *
+ * @param {Object} container
+ *
+ * @returns {string}
+ */
+export function getPayloadHash(container: any): string {
+  return keccak256(
+    solidityPack(
+      [
+        'uint256',
+        'uint256',
+        'address',
+        'address',
+        'bytes32',
+        'bytes32',
+        'bytes32',
+      ],
+      [
+        container.payload.nonce,
+        container.payload.executionTime,
+        container.payload.submitter,
+        container.payload.executor,
+        keccak256(
+          defaultAbiCoder.encode(
+            [
+              'tuple(' +
+              'address to, ' +
+              'uint256 value, ' +
+              'bytes data' +
+              ')[]',
+            ],
+            [container.payload.actions]
+          )
+        ),
+        container.payload.allowFailuresMap,
+        keccak256(container.payload.proof),
+      ]
+    )
+  )
+}
+
+/**
+ * Returns the hash of the container config
+ *
+ * @param {Object} container
+ *
+ * @returns {string}
+ */
+export function getConfigHash(container: any): string {
+  return keccak256(
+    defaultAbiCoder.encode(
+      [
+        'tuple(' +
+        'uint256 executionDelay, ' +
+        'tuple(address token, uint256 amount) scheduleDeposit, ' +
+        'tuple(address token, uint256 amount) challengeDeposit, ' +
+        'address resolver, ' +
+        'bytes rules' +
+        ')',
+      ],
+      [container.config]
+    )
+  )
+}
+
+/**
+ * Returns the hash of the container
+ *
+ * @param {Object} container
+ * @param {string} address
+ * @param {string} chainId
+ *
+ * @returns {string}
+ */
+export function getContainerHash(container: any, address: string, chainId: number): string {
+  return keccak256(
+    solidityPack(
+      ['string', 'address', 'uint', 'bytes32', 'bytes32'],
+      ['erc3k-v1', address, chainId, getPayloadHash(container), getConfigHash(container)]
+    )
+  )
+}
