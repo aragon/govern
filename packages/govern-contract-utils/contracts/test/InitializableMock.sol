87f5c268b89285aea483c5c8061de5174c91ca11
Oliver <onbjerg@users.noreply.github.com>
2020-11-02 14:59:05 +0100

    Utils: Initializable tests (#165)

---
+++ packages/govern-contract-utils/contracts/test/InitializableMock.sol
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier:    GPL-3.0
+ */
+
+pragma solidity 0.6.8;
+
+import "../initializable/Initializable.sol";
+
+contract InitializableMock is Initializable {
+    function initOne() external onlyInit("one") {}
+    
+    function initTwo() external onlyInit("two") {}
+}
