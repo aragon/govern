62e909262b57457e2f981743e2a0d9533ee14599
Gabi <gabrielpk.18@gmail.com>
2020-11-05 12:12:54 -0300

    Create: delete buidler js file

--- packages/govern-create/lib/utils.js
+++
@@ -1,6 +0,0 @@
-const format = ({ address }, name) =>
-  `- ${name}: https://${process.env.MAINNET ? '' : 'rinkeby.'}etherscan.io/address/${address}`
-
-const print = (contract, name) => console.log(format(contract, name))
-
-module.exports = { format, print }
