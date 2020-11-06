43e14287df4b742a0eb1ec0da8e5b13180605f79
Martynas Prokopas <promaty@gmail.com>
2020-11-06 18:50:55 +0100

    Server: fixed dockerfile to exclude erc3kjs package.json

--- .github/scripts/kubectl-wait-ready.sh
+++ .github/scripts/kubectl-wait-ready.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e # exit on error
 APP="$1"
 # wait 5 min for the new pod to be ready. If the pod is not ready there is a problem with the new container
 kubectl wait pod --for condition=Ready --timeout=300s $(kubectl get pods -l app=$APP --sort-by {.metadata.creationTimestamp} -o jsonpath={.items[-1].metadata.name})
