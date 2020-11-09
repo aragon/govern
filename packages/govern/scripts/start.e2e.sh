5cc4779b322a651e231d44e7a296dbca603780ae
nivida <samuel@ethereum.org>
2020-11-09 14:20:35 +0100

    requested changes from @promaty implemented

--- packages/govern/scripts/start.e2e.sh
+++ packages/govern/scripts/start.e2e.sh
@@ -5,7 +5,7 @@ cd ../govern-server/
 nohup yarn dev >>/dev/null 2>>/dev/null &
 
 # Check if the govern-server is running
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:3000/.well-known/apollo/server-health)" != "200" ]]; do sleep 5; done
+yarn server:healthcheck
 
 # Go to govern package and start e2e tests
 cd ../govern
