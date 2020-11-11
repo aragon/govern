d4e8a4269849cd47b424aa4041033dbbe8f51f74
Gabi <gabrielpk.18@gmail.com>
2020-11-11 14:26:47 -0300

    Create: remove dev-env log statement

--- packages/govern-create/tasks/deployments/dev-deploy.ts
+++ packages/govern-create/tasks/deployments/dev-deploy.ts
@@ -1,6 +1,5 @@
 import { task } from 'hardhat/config'
 import { setHRE, HRE } from '../../helpers/helpers'
-import { logInfo } from '../../helpers/logger'
 
 task('dev-deploy', 'Full deployment flow on hardhat network').setAction(
   async (_, _HRE) => {
@@ -10,7 +9,5 @@ task('dev-deploy', 'Full deployment flow on hardhat network').setAction(
     await run(`deploy-registry`)
     await run(`deploy-factory`)
     await run(`deploy-govern`)
-
-    logInfo(`- Finished deployment at ${HRE.network.name} network.`)
   }
 )
