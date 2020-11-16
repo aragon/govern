a0f9161ca311cbaa919823ac239edfc88708341b
nivida <samuel@ethereum.org>
2020-11-16 15:26:34 +0100

    draft fastify auth plugin installed

--- packages/govern-tx/src/auth/auth-plugin.ts
+++ packages/govern-tx/src/auth/auth-plugin.ts
@@ -1,35 +1,39 @@
 import fp from 'fastify-plugin'
 import Authenticator from './Authenticator'
 import { Unauthorized } from 'http-errors'
+import fastifyCookie from 'fastify-cookie'
 
 export interface AuthOptions {
     authenticator: Authenticator,
-    secret: string
+    secret: string,
+    cookieName: string
 }
 
 
 function authPlugin(fastify, options: AuthOptions, next) {
     const authenticator = options.authenticator
     const secret = options.secret
-
+    fastify.register(fastifyCookie)
     fastify.decorate('pkAuth', auth)
 
     next()
 
     function auth(request, reply, next) {
-        if (request.header.autorization && authenticator.verify(request.header.autorization)) {
+        if (authenticator.verify(request.cookies[options.cookieName])) {
             next()
+
             return
         }
         
         const body = JSON.parse(request.body)
         const token = authenticator.authenticate(body.message, body.signature)
         if (!token) {
-            reply()
+            next(new Unauthorized('Unknown account!'))
             return
         }
-
-        next(new Unauthorized('Unknown account!'))
+        
+        reply.setCookie(request.cookies[options.cookieName], token, {secure: true})
+        next()
     }
 }
 
