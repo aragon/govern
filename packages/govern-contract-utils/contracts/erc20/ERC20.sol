9e97c5f87942ba268aa1b7896546df8c3e0927fd
Brett Sun <qisheng.brett.sun@gmail.com>
2020-12-07 23:53:50 +0100

    govern-contract-utils: update ERC20 abstract contract into interface

--- packages/govern-contract-utils/contracts/erc20/ERC20.sol
+++ packages/govern-contract-utils/contracts/erc20/ERC20.sol
@@ -8,21 +8,18 @@ pragma solidity ^0.6.8;
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
-abstract contract ERC20 {
-    function totalSupply() virtual public view returns (uint256);
+interface ERC20 {
+    function totalSupply() external view returns (uint256);
 
-    function balanceOf(address _who) virtual public view returns (uint256);
+    function balanceOf(address _who) external view returns (uint256);
 
-    function allowance(address _owner, address _spender)
-        virtual public view returns (uint256);
+    function allowance(address _owner, address _spender) external view returns (uint256);
 
-    function transfer(address _to, uint256 _value) virtual public returns (bool);
+    function transfer(address _to, uint256 _value) external returns (bool);
 
-    function approve(address _spender, uint256 _value)
-        virtual public returns (bool);
+    function approve(address _spender, uint256 _value) external returns (bool);
 
-    function transferFrom(address _from, address _to, uint256 _value)
-        virtual public returns (bool);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool);
 
     event Transfer(
         address indexed from,
