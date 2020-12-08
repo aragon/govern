6e78297783e67d425858a2f66bf11971284e80a0
Brett Sun <qisheng.brett.sun@gmail.com>
2020-12-08 15:38:54 +0100

    govern-contract-utils: lightly update DepositLib's event and error API (#251)

--- packages/govern-contract-utils/contracts/deposits/DepositLib.sol
+++ packages/govern-contract-utils/contracts/deposits/DepositLib.sol
@@ -12,24 +12,24 @@ import "../erc20/SafeERC20.sol";
 library DepositLib {
     using SafeERC20 for ERC20;
 
-    event Lock(address indexed token, address indexed from, uint256 amount);
-    event Unlock(address indexed token, address indexed to, uint256 amount);
+    event Locked(address indexed token, address indexed from, uint256 amount);
+    event Unlocked(address indexed token, address indexed to, uint256 amount);
 
     function collectFrom(ERC3000Data.Collateral memory _collateral, address _from) internal {
         if (_collateral.amount > 0) {
             ERC20 token = ERC20(_collateral.token);
-            require(token.safeTransferFrom(_from, address(this), _collateral.amount), "queue: bad get token");
+            require(token.safeTransferFrom(_from, address(this), _collateral.amount), "deposit: bad token lock");
 
-            emit Lock(_collateral.token, _from, _collateral.amount);
+            emit Locked(_collateral.token, _from, _collateral.amount);
         }
     }
 
     function releaseTo(ERC3000Data.Collateral memory _collateral, address _to) internal {
         if (_collateral.amount > 0) {
             ERC20 token = ERC20(_collateral.token);
-            require(token.safeTransfer(_to, _collateral.amount), "queue: bad send token");
+            require(token.safeTransfer(_to, _collateral.amount), "deposit: bad token release");
 
-            emit Unlock(_collateral.token, _to, _collateral.amount);
+            emit Unlocked(_collateral.token, _to, _collateral.amount);
         }
     }
 }
