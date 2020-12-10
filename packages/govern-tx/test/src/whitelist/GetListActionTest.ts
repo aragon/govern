b68dca78da597d321690b58628b2e80c848a6c33
nivida <samuel@ethereum.org>
2020-12-10 11:06:01 +0100

    GetListActionTest implemented

---
+++ packages/govern-tx/test/src/whitelist/GetListActionTest.ts
@@ -0,0 +1,46 @@
+import { isAddress } from '@ethersproject/address';
+import { WhitelistRequest } from '../../../lib/whitelist/AbstractWhitelistAction';
+import Database from '../../../src/db/Database';
+import Whitelist, { ListItem } from '../../../src/db/Whitelist';
+import GetListAction from '../../../src/whitelist/GetListAction';
+
+// Mocks
+jest.mock('../../../src/db/Whitelist')
+jest.mock('@ethersproject/address')
+
+/**
+ * GetListAction test
+ */
+describe('GetListAction Test', () => {
+    let getListAction: GetListAction,
+    whitelistMock: Whitelist
+
+    beforeEach(() => {
+        new Whitelist({} as Database)
+        whitelistMock = (Whitelist as jest.MockedClass<typeof Whitelist>).mock.instances[0]
+    })
+
+    it('calls execute and returns the expected result', async () => {
+        (isAddress as jest.MockedFunction<typeof isAddress>).mockReturnValueOnce(true);
+
+        (whitelistMock.getList as jest.MockedFunction<typeof whitelistMock.getList>).mockReturnValueOnce(Promise.resolve([{}] as ListItem[]));
+        
+        getListAction = new GetListAction(whitelistMock)
+        
+        await expect(getListAction.execute()).resolves.toEqual([{}])
+
+        expect(whitelistMock.getList).toHaveBeenNthCalledWith(1)
+    })
+
+    it('calls execute and throws as expected', async () => {
+        (isAddress as jest.MockedFunction<typeof isAddress>).mockReturnValueOnce(true);
+
+        (whitelistMock.getList as jest.MockedFunction<typeof whitelistMock.getList>).mockReturnValueOnce(Promise.reject('NOPE'));
+        
+        getListAction = new GetListAction(whitelistMock)
+        
+        await expect(getListAction.execute()).rejects.toEqual('NOPE')
+
+        expect(whitelistMock.getList).toHaveBeenNthCalledWith(1)
+    })
+})
