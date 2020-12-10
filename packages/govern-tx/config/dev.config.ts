025b6088b23394753687c4f1757ad842c504fe13
nivida <samuel@ethereum.org>
2020-12-10 13:04:23 +0100

    config setup implemented

---
+++ packages/govern-tx/config/dev.config.ts
@@ -0,0 +1,21 @@
+export const config = {
+    ethereum: {
+        publicKey: '', // Add default EOA
+        contracts: {
+            GovernQueue: '' // Add deployment address can get calculated if deployed with create2
+        },
+        url: 'localhost:8545',
+        blockConfirmations: 42
+    }, 
+    database: {
+        user: 'govern-tx',
+        host: 'localhost',
+        password: 'tx-service',
+        database: 'govern-tx',
+        port: 5432
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 4040
+    }
+} 
--- packages/govern-tx/src/index.ts
+++ packages/govern-tx/src/index.ts
@@ -1,28 +1,14 @@
-import Configuration from './config/Configuration'
+import { config as prodConfig } from '../config/prod.config'
+import { config as devConfig } from '../config/dev.config'
+import Configuration, { Config } from './config/Configuration'
 import Bootstrap from './Bootstrap'
 
-new Bootstrap(
-    new Configuration(
-        {
-            ethereum: {
-                publicKey: '0x0...',
-                contracts: {
-                    GovernQueue: '0x0...'
-                },
-                url: 'localhost:8545',
-                blockConfirmations: 42
-            }, 
-            database: {
-                user: 'govern',
-                host: 'localhost',
-                password: 'dev',
-                database: 'govern',
-                port: 4000
-            },
-            server: {
-                host: '0.0.0.0',
-                port: 4040
-            }
-        }   
-    )
-).run()
+let config: Config;
+
+if (process.env.DEV) {
+    config = devConfig
+} else {
+    config = prodConfig
+}
+
+new Bootstrap(new Configuration(config)).run()
