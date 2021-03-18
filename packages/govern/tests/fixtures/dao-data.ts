206565b872d4c2b64fd9066ff9e171ab72c7a72f
Yuet Loo Wong <contact@yuetloo.com>
2021-03-17 23:17:51 -0400

    run all govern tests

--- packages/govern/tests/fixtures/dao-data.ts
+++ packages/govern/tests/fixtures/dao-data.ts
@@ -1,5 +1,6 @@
 const name = 'bitconnect'
 const queueAddress = '0xf285a1227ba8f3cdb6d612d9cae125af62b951d1'
+
 const daoData = {
    name,
    address: '0x0b834c0948491ad369d30fc1bff9bc299777632e',
