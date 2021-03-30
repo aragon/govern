160dfbe0a491384b3eefbf61e56ca2304551801d
Yuet Loo Wong <contact@yuetloo.com>
2021-03-30 17:58:31 -0400

    remove obsolete test data

--- packages/govern/tests/e2e/config.ts
+++ packages/govern/tests/e2e/config.ts
@@ -1,2 +1,1 @@
 export const subgraphURL = 'https://api.thegraph.com/subgraphs/name/aragon/aragon-govern-rinkeby'
-//export const subgraphURL = 'https://graph.backend.aragon.org/subgraphs/name/aragon/aragon-govern-mainnet'
