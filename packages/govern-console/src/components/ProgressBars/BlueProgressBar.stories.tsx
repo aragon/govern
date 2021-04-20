d937161b57e679bcdfe6c7b3e4f05dc25774b804
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2021-04-20 15:13:49 +0200

    blue porgressbar component

---
+++ packages/govern-console/src/components/ProgressBars/BlueProgressBar.stories.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable */
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+
+import { Story, Meta } from '@storybook/react';
+
+import { BlueProgressBar } from './BlueProgressBar';
+import { LinearProgressProps } from '@material-ui/core/LinearProgress';
+
+export default {
+  title: 'Blue-progressBar',
+  component: BlueProgressBar,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta;
+
+const Template: Story<LinearProgressProps> = (args) => <BlueProgressBar {...args}/>;
+
+export const blue_progress_bar = Template.bind({});
+
+blue_progress_bar.args = {
+    value: 50,
+  };
