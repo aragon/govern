00aa07ac1e91156325115b939f837bf3e28817d6
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2021-05-04 16:40:41 +0400

    add vocdoni dvote

---
+++ packages/govern/tests/hardhat/Gateway.test.ts
@@ -0,0 +1,16 @@
+import { getPool } from '../../internal/actions/lib/Gateway'
+import { providers } from 'ethers'
+import { expect } from 'chai'
+
+describe('GatewayTest', function() {
+  it('getPool with provider should work', async function() {
+    const provider = new providers.InfuraProvider('rinkeby')
+    const pool = await getPool(provider)
+    expect(pool).to.have.property('isReady')
+  })
+
+  it('getPool without provider should work', async function() {
+    const pool = await getPool()
+    expect(pool).to.have.property('isReady')
+  })
+})
