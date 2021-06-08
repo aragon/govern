c552aadde6062fcba6a38e552365d324b9456797
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2021-06-08 14:34:55 +0200

    add aragon ui to home page

--- packages/govern-console/src/containers/HomePage/HomePage.tsx
+++ packages/govern-console/src/containers/HomePage/HomePage.tsx
@@ -1,66 +1,59 @@
 import { Switch, Route } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
-import { styled } from '@material-ui/core/styles';
+// import { styled } from '@material-ui/core/styles';
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
 import ConsoleMainPage from 'containers/Console/ConsoleMainPage';
 import DaoMainPage from 'containers/DAO/DaoMainPage';
 import ProposalDetails from 'containers/ProposalDetails/ProposalDetails';
 import NewProposal from 'containers/NewProposal/NewProposal';
-import CreateDao from 'containers/CreateDAO/CreateDao';
+import CreateDaoOld from 'containers/CreateDAO_old/CreateDao';
 import DaoSettings from 'containers/DAOSettings/DAOSettings';
 import { ModalsProvider } from 'containers/HomePage/ModalsContext';
-
-const AppWrapper = styled('div')({
-  width: 'calc(100vw - 96px)',
-  maxWidth: '1440px',
-  margin: 'auto',
-  boxSizing: 'border-box',
-  position: 'relative',
-  minHeight: '100vh',
-});
-
-const MainBodyWrapper = styled('div')({
-  width: '100%',
-  minHeight: 'calc(100vh - 212px)',
-  boxSizing: 'border-box',
-  top: '106px',
-  overflowX: 'hidden',
-  overflowY: 'auto',
-});
+import { Main } from '@aragon/ui';
+import CreateDao from '../CreateDao/CreateDao';
+import { useState } from 'react';
 
 const HomePage = () => {
   const history = useHistory();
+  const [themeMode, setThemeMode] = useState('light');
+  const toggleThemeMode = () => {
+    if (themeMode === 'light') {
+      setThemeMode('dark');
+    } else {
+      setThemeMode('light');
+    }
+  };
 
   return (
     <ModalsProvider>
-      <AppWrapper id="app-wrapper">
-        <Header />
-        {/* add breadcrumbs later */}
-        <MainBodyWrapper id="main-body-wrapper">
-          <Switch>
-            <Route exact path="/">
-              <ConsoleMainPage />
-            </Route>
-            <Route exact path="/daos/:daoName">
-              <DaoMainPage />
-            </Route>
-            <Route exact path="/proposals/:daoName/:id">
-              <ProposalDetails onClickBack={() => history.goBack()} />
-            </Route>
-            <Route exact path="/daos/:daoName/new-proposal">
-              <NewProposal onClickBack={() => history.goBack()} />
-            </Route>
-            <Route exact path="/daos/:daoName/dao-settings">
-              <DaoSettings onClickBack={() => history.goBack()} />
-            </Route>
-            <Route exact path="/create-dao">
-              <CreateDao />
-            </Route>
-          </Switch>
-        </MainBodyWrapper>
+      <Main theme={themeMode}>
+        <Header toggleTheme={toggleThemeMode} themeMode={themeMode} />
+        <Switch>
+          <Route exact path="/">
+            <ConsoleMainPage />
+          </Route>
+          <Route exact path="/daos/:daoName">
+            <DaoMainPage />
+          </Route>
+          <Route exact path="/proposals/:daoName/:id">
+            <ProposalDetails onClickBack={() => history.goBack()} />
+          </Route>
+          <Route exact path="/daos/:daoName/new-proposal">
+            <NewProposal onClickBack={() => history.goBack()} />
+          </Route>
+          <Route exact path="/daos/:daoName/dao-settings">
+            <DaoSettings onClickBack={() => history.goBack()} />
+          </Route>
+          <Route exact path="/create-dao-old">
+            <CreateDaoOld />
+          </Route>
+          <Route exact path="/create-dao">
+            <CreateDao />
+          </Route>
+        </Switch>
         <Footer />
-      </AppWrapper>
+      </Main>
     </ModalsProvider>
   );
 };
