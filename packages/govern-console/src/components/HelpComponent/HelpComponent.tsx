9f398d5e2523785bf918c2897d6d0b3ad6bff64e
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2021-07-02 18:38:47 +0200

    Feature/dao 418 create help component for console (#420)
    * starting help component
    
    * adding image
    
    * add helpComponent to CreateDao and Setting
    
    * subgraph fixed
    
    * address list correction
    
    * correct parttern for pages
    
    * show correct view on progress
    
    Co-authored-by: Giorgi Lagidze <glagh14@freeuni.edu.ge>

---
+++ packages/govern-console/src/components/HelpComponent/HelpComponent.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Box, Button, StyledText, useLayout, useTheme, Grid, GridItem, GU } from '@aragon/ui';
+import helpFaceImage from 'images/pngs/help_face_@2x.png';
+import { ARAGON_HELP_URL } from '../../utils/constants';
+
+function HelpComponent() {
+  const theme = useTheme();
+  const { layoutName } = useLayout();
+  return (
+    <Box style={{ border: 'none', backgroundColor: '#8991FF1A' }}>
+      <Grid columns={'4'} columnWidth={'1fr'} gap={14}>
+        <GridItem gridColumn={'1/2'} alignHorizontal={layoutName !== 'large' && 'center'}>
+          <img
+            src={helpFaceImage}
+            style={{ width: 76, height: 48, marginTop: layoutName === 'large' ? 3 * GU : GU }}
+          />
+        </GridItem>
+        <GridItem gridColumn={'2/5'}>
+          <StyledText name="title3">Need help?</StyledText>
+          <StyledText name="body2" style={{ color: theme.disabledContent }}>
+            Ask us anything, or share your feedback
+          </StyledText>
+        </GridItem>
+      </Grid>
+
+      <Button
+        wide
+        type="primary"
+        size={layoutName}
+        style={{ background: theme.purple, marginTop: 2 * GU }}
+        onClick={() => window.open(ARAGON_HELP_URL)}
+      >
+        Visit our help center
+      </Button>
+    </Box>
+  );
+}
+
+export default HelpComponent;
