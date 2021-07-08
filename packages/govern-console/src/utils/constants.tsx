485d5a2a1550e818858802d0a3d4fe5caa95f1c5
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2021-07-08 19:29:56 +0400

    ipfs gateway link fixed

--- packages/govern-console/src/utils/constants.tsx
+++ packages/govern-console/src/utils/constants.tsx
@@ -4,6 +4,6 @@ export const PROXY_CONTRACT_URL = 'https://eips.ethereum.org/EIPS/eip-1167';
 
 export const INFURA_PROJECT_ID = '7a03fcb37be7479da06f92c5117afd47';
 export const ETHERSCAN_API_KEY = 'HS2AR94Q6G6RYIYHR7VRCZGP6E382K4BDC ';
-export const IPFS_GATEWAY = 'https://ipfs.io/ipfs';
+export const IPFS_GATEWAY = 'https://ipfs.io/ipfs/';
 export const ARAGON_HELP_URL = 'https://help.aragon.org/';
 export const MAX_SCHEDULE_ACCESS_LIST_ALLOWED = 10;
