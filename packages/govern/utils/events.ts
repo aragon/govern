5bdfe05513de3e2fee0c259866f697c08906d91c
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2021-08-03 11:20:13 +0200

    cancel previouse subscriptions

--- packages/govern/utils/events.ts
+++ packages/govern/utils/events.ts
@@ -1,7 +1,7 @@
 import { Contract, providers, Signer } from 'ethers'
 
 const registryAbi = [
-    `event Registered(
+  `event Registered(
         address indexed executor, 
         address queue, 
         address indexed token, 
@@ -11,27 +11,39 @@ const registryAbi = [
     )`,
 ]
 
-let currentRegistryAddress = null;
+// let currentRegistryAddress = null
+let currentGovernRegistry = null
 
-export function setUpRegisteredEvent(registryAddress: string, signer: providers.Provider | Signer, fn: Function, daoName: string) {
-    // if the registry hasn't changed, no need to register it again..
-    if(currentRegistryAddress && currentRegistryAddress == registryAddress) {
-        return;
+export function setUpRegisteredEvent(
+  registryAddress: string,
+  signer: providers.Provider | Signer,
+  fn: Function,
+  daoName: string
+) {
+  // if the registry hasn't changed, no need to register it again..
+  //   if (currentRegistryAddress && currentRegistryAddress == registryAddress) {
+  //     return
+  //   }
+
+  let GovernRegistry =
+    currentGovernRegistry || new Contract(registryAddress, registryAbi, signer)
+
+  currentGovernRegistry = GovernRegistry
+
+  //   currentRegistryAddress = registryAddress
+
+  const listeners = GovernRegistry.listeners('Registered')
+  if (listeners.length) {
+    // clean previouse listeners
+    listeners.forEach((listener) => GovernRegistry.off('Registered', listener))
+  }
+  GovernRegistry.on(
+    'Registered',
+    async (excecutor, queue, token, minter, registrant, name) => {
+      // not our DAO, wait for next one
+      if (name !== daoName) return
+      // send back token address and excecutor
+      fn(token, excecutor)
     }
-
-    let GovernRegistry = new Contract(
-        registryAddress,
-        registryAbi,
-        signer
-    )
-    currentRegistryAddress = registryAddress
-    GovernRegistry.on(
-        'Registered',
-        async (excecutor, queue, token, minter, registrant, name) => {
-            // not our DAO, wait for next one
-            if (name !== daoName) return
-            // send back token address            
-            fn(token, excecutor)
-        }
-    )
+  )
 }
