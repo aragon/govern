aea9cf5ca55b1f084f9f44e66ec4d77c632f317c
Sepehr sanaee <sepehr_sanaee78@yahoo.com>
2021-09-27 23:47:47 +0330

    add finance page header

--- packages/govern-console/src/containers/DAO/DaoFinancePage.tsx
+++ packages/govern-console/src/containers/DAO/DaoFinancePage.tsx
@@ -1,5 +1,85 @@
+import React, { useState } from 'react';
+import { Button, GU, useLayout, IconDown, Grid, GridItem } from '@aragon/ui';
+import styled from 'styled-components';
+import DaoTransactionCard from './components/DaoTransactionCard/DaoTransactionCard';
+
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: ${3 * GU}px;
+`;
+
+const Title = styled.p`
+  font-weight: 600;
+  font-size: 28px;
+  line-height: 125%;
+`;
+
+const CustomActionButton = styled(Button)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 24px;
+  width: 134px;
+  height: 44px;
+  border-radius: 12px;
+  box-shadow: none;
+`;
+
+const TransactionListContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: ${3 * GU}px;
+`;
+
+const ListTitle = styled.p`
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 125%;
+  margin-bottom: 16px;
+`;
+
+const LoadMoreButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 24px;
+  width: 154px;
+  height: 44px;
+  border-radius: 12px;
+  color: #7483ab;
+  background: #ffffff;
+  box-shadow: none;
+  cursor: pointer;
+  & > svg {
+    padding-left: 2px;
+  }
+`;
+
 const DaoFinancePage: React.FC = () => {
-  return <div>Hello Finances!</div>;
+  const { layoutName } = useLayout();
+  return (
+    <div>
+      <HeaderContainer>
+        <Title>Finance</Title>
+        <CustomActionButton label="New Transfer" />
+      </HeaderContainer>
+      <TransactionListContainer>
+        <ListTitle>Transactions</ListTitle>
+        <DaoTransactionCard />
+      </TransactionListContainer>
+      <LoadMoreButton>
+        <span>Load more</span>
+        <IconDown />
+      </LoadMoreButton>
+    </div>
+  );
 };
 
 export default DaoFinancePage;
