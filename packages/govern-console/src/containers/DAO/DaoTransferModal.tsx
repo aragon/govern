3e019ac5970c33182b4a76f8bc1f50c6ac54e66c
sepehr <sepehr2mail@gmail.com>
2021-09-28 15:43:36 +0330

    SelectToken modal added

--- packages/govern-console/src/containers/DAO/DaoTransferModal.tsx
+++ packages/govern-console/src/containers/DAO/DaoTransferModal.tsx
@@ -1,15 +1,21 @@
 import { Modal } from '@aragon/ui';
 import styled from 'styled-components';
+import SelectToken from './components/ModalContents/SelectToken';
 
 const TransferModal = styled(Modal)`
-  border-radius: 16px;
+  & > div > div > div {
+    border-radius: 16px;
+    background: #f6f9fc;
+    max-height: 768px;
+    overflow: auto;
+  }
 `;
 
 const DaoTransferModal: React.FC<{ opened: boolean; close: () => void }> = ({ opened, close }) => {
   return (
     <>
       <TransferModal visible={opened} onClose={close}>
-        modal content
+        <SelectToken />
       </TransferModal>
     </>
   );
