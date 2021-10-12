d35ea60f1fa59ca99d8c8fd57a722a5083ea6ec1
Fabrice Francois <fabricevladimir@gmail.com>
2021-10-12 09:28:25 -0400

    cleaned up useEffect

--- packages/govern-console/src/containers/DAO/components/FinanceSideCard/FinanceSideCard.tsx
+++ packages/govern-console/src/containers/DAO/components/FinanceSideCard/FinanceSideCard.tsx
@@ -94,6 +94,8 @@ const FinanceSideCard: React.FC<Props> = ({ tokens, mainToken, onNewTransfer }) 
 
   const { layoutName } = useLayout();
   const layoutIsSmall = useMemo(() => layoutName === 'small', [layoutName]);
+
+  // TODO: remove when implementing skeleton loading
   const { symbol, amount, price, numberOfAssets } = useMemo(() => {
     return {
       amount: tokens[mainToken]?.amountForHuman || 0.0,
@@ -105,13 +107,8 @@ const FinanceSideCard: React.FC<Props> = ({ tokens, mainToken, onNewTransfer }) 
 
   useEffect(() => {
     // Assets shown on medium to large screens
-    if (!layoutIsSmall) {
-      setDisplayAssets(true);
-    }
-
-    if (layoutIsSmall) {
-      setDisplayAssets(false);
-    }
+    if (!layoutIsSmall) setDisplayAssets(true);
+    else setDisplayAssets(false);
   }, [layoutIsSmall]);
 
   function sortAssets(asset: any, nextAsset: any) {
