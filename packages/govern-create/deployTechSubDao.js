072e6ee39eee64e84e0aaca3d538292c034f40e7
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2021-12-09 17:11:12 +0100

    cleaning

--- packages/govern-create/deployTechSubDao.js
+++ packages/govern-create/deployTechSubDao.js
@@ -1,48 +1,10 @@
-const { ethers } = require("ethers");
-const { LedgerSigner } = require("@ethersproject/hardware-wallets");
-const TechSubDAOFactoryArtifacts = require("./artifacts/contracts/TechSubDAOFactory.sol/TechSubDAOFactory.json");
+const { ethers, BigNumber } = require('ethers')
+const { LedgerSigner } = require('@ethersproject/hardware-wallets')
+const TechSubDAOFactoryArtifacts = require('./artifacts/contracts/TechSubDAOFactory.sol/TechSubDAOFactory.json')
 
-const provider = new ethers.providers.InfuraProvider("mainnet", process.env.ETHERSCAN_KEY);
-//const signer = (new ethers.Wallet("", provider)).connect(provider);
-const signer = new LedgerSigner(provider)
-
-const ANMainDAOAddress = '0x9c1d24318966793a68e6005eb6b27edace3f28b8'
-
-async function deployTechSubDAOFactory() {
-  const TechSubDaoFactoryFactory = new ethers.ContractFactory(
-    TechSubDAOFactoryArtifacts.abi,
-    TechSubDAOFactoryArtifacts.bytecode,
-    signer
-  )
-
-  let TechSubDaoFactory = await TechSubDaoFactoryFactory.deploy(
-    "0xf2b7d096cd34f228a6413e276132c21d98b19882", // GovernRegistry
-    "0x6090afbdcf13b3a53323509955279ce0de4003a2", // GovernFactory
-    "0x5d2d6a91c8b5fb62f2a6725eb791f412a5d39c4d", // GovernQueueFactory
-    ANMainDAOAddress,
-    {
-      maxFeePerGas: ethers.utils.parseUnits('120.0', 'gwei'),
-      maxPriorityFeePerGas: ethers.utils.parseUnits('1.5', 'gwei'),
-      gasLimit: 10000000
-    }
-  )
-  await TechSubDaoFactory.deployed();
-
-  console.log("TechSubDaoFactory Deployed:", TechSubDaoFactory.address)
-
-  return TechSubDaoFactory;
-}
-
-async function deployTechSubDao(TechSubDaoFactory) {
-    TechSubDaoFactory.on("SubDAOsDeployed", console.log)
-
-    await TechSubDaoFactory.deployTechSubDao()
-}
-
-async function deployTechSubDaoFactoryAndTechSubDao() {
-    const techSubDaoFactory = await deployTechSubDAOFactory();
-    await deployTechSubDao(techSubDaoFactory);
-}
+const provider = new ethers.providers.InfuraProvider('rinkeby', 'c6a4d4f3dfbd4f78825ad73083620404')
+const signer = (new ethers.Wallet('', provider)).connect(provider);
+//const signer = new LedgerSigner(provider)
 
 async function printInfos() {
   blockNumber = await provider.getBlockNumber()
@@ -53,13 +15,55 @@ async function printInfos() {
   console.log(
     '\nBlock Number         : ' + blockNumber,
     '\nAddress              : ' + address,
-    '\nBalance              : ' +ethers.utils.formatEther(balance) + ' ETH',
+    '\nBalance              : ' + ethers.utils.formatEther(balance) + ' ETH',
     '\nmaxFeePerGas         : ' + ethers.utils.formatUnits(feeData.maxFeePerGas, 'gwei') + ' gwei',
     '\nmaxPriorityFeePerGas : ' + ethers.utils.formatUnits(feeData.maxPriorityFeePerGas, 'gwei') + ' gwei',
     '\ngasPrice             : ' + ethers.utils.formatUnits(feeData.gasPrice, 'gwei') + ' gwei'
   )
 }
 
-printInfos()
+const options = {
+  maxFeePerGas: ethers.utils.parseUnits('50.0', 'gwei'),
+  maxPriorityFeePerGas: ethers.utils.parseUnits('1.5', 'gwei'),
+  gasLimit: ethers.BigNumber.from(1.5 * 10 ** 6)
+}
+const transactionPrice = ethers.utils.formatEther(options.gasLimit.mul(options.maxFeePerGas))
 
-deployTechSubDaoFactoryAndTechSubDao().catch(console.log);
+console.log(`Make sure to have ${transactionPrice} ETH in your wallet for each of the two transaction.`)
+
+async function deployTechSubDAOFactory() {
+  const TechSubDaoFactoryFactory = new ethers.ContractFactory(
+    TechSubDAOFactoryArtifacts.abi,
+    TechSubDAOFactoryArtifacts.bytecode,
+    signer,
+    options
+  )
+
+  let TechSubDaoFactory = await TechSubDaoFactoryFactory.deploy(
+    '0xf2b7D096cd34F228A6413e276132C21D98b19882', // GovernRegistry
+    '0x6090AfBDCF13b3a53323509955279Ce0De4003a2', // GovernFactory
+    '0x5d2d6A91c8b5fB62f2A6725eb791f412A5d39C4d', // GovernQueueFactory
+    '0x9C1d24318966793A68e6005eB6B27EDacE3f28B8', // ANMainDAOAddress
+    options
+  )
+  await TechSubDaoFactory.deployed()
+
+  console.log('TechSubDaoFactory Deployed:', TechSubDaoFactory.address)
+
+  return TechSubDaoFactory
+}
+
+async function deployTechSubDao(TechSubDaoFactory) {
+  TechSubDaoFactory.on('TechSubDAODeployed', console.log)
+
+  await TechSubDaoFactory.deployTechSubDao(options)
+}
+
+async function deployTechSubDaoFactoryAndTechSubDao() {
+  const techSubDaoFactory = await deployTechSubDAOFactory()
+  await deployTechSubDao(techSubDaoFactory)
+}
+
+
+printInfos()
+//deployTechSubDaoFactoryAndTechSubDao().catch(console.log);
