800901cfe5767c8e25676c5826f2ede9e475f724
Marc Velmer <marc1987@gmail.com>
2022-04-12 19:59:55 +0200

    Fixed DAO Creation issues (#535)
    Updated `TS` version
    Updated `ethers` library version
    Removed `dvote-js` library, instead use `client` and `census` packages
    Modified source code for `dvote-js` calls (breaking changes applied)

--- packages/govern/internal/actions/RegisterToken.ts
+++ packages/govern/internal/actions/RegisterToken.ts
@@ -1,4 +1,4 @@
-import { CensusErc20Api } from 'dvote-js'
+import { CensusErc20Api } from '@vocdoni/census'
 import { getPool } from './lib/Gateway'
 import { ContractReceipt, Wallet, Signer } from 'ethers'
 
@@ -13,7 +13,8 @@ export const isTokenRegistered = async (
   tokenAddress: string
 ): Promise<boolean> => {
   const pool = await getPool(signer.provider)
-  return await CensusErc20Api.isRegistered(tokenAddress, pool)
+  return await CensusErc20Api.getTokenInfo(tokenAddress, pool)
+    .then(tokenInfo => tokenInfo.isRegistered)
 }
 
 /**
